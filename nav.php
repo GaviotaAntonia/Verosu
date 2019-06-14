f6493c7a09bf68f62d9869532f30de8faf54070b
Gaviota Antonia Herrera Prieto <35613309+GaviotaAntonia@users.noreply.github.com>
2019-06-14 16:46:59 -0500

    Pagina 1

---
+++ nav.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="footer, links, icons" />
+
+	<title>Distributed Footer</title>
+
+	<link rel="stylesheet" href="css/masestilos.css">
+	<link rel="stylesheet" href="css/estilos.css">
+	
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+
+</head>
+
+	<body>
+
+		<!--header-->
+  <header id="header" class=""><nav class="navbar navbar-expand-md bg-dark navbar-dark fixed-top">
+	<img src="PRUEBA3.png" alt="logos" style="text-align: right;>
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      
+      <!-- Dropdown Servicio -->
+        <li class="nav-item dropdown">
+           <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">Servicio
+            </a>
+            <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">1</a>
+            <a class="dropdown-item" href="#">Link 2</a>
+            <a class="dropdown-item" href="#">Link 3</a>
+            </div>
+        </li>
+      <!--Final Dropdown Servicio -->
+
+      <form class="form-inline">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>    
+    </ul>
+  </div>  
+  </nav>
+  </header>
+<!-- Fin de header -->
+	</body>
+
+</html>
