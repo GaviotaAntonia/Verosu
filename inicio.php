6f86293aa239579559cb8b248abecc332c52544b
Gaviota Antonia Herrera Prieto <35613309+GaviotaAntonia@users.noreply.github.com>
2019-06-17 12:45:54 -0500

    Cambios en nav,y se agregaron otros archivos

--- inicio.php
+++ inicio.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -10,6 +9,9 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   <link rel="shortcut icon" type="image/png" href="conphoto/ab.png"> 
+  
+  <link rel="stylesheet" href="css/masestilos.css">
+  <link rel="stylesheet" href="css/estilos.css">
   <!--LETRA QUE SE UTLIZA-->
 <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
 
@@ -23,108 +25,86 @@
 <body>
 
 <img src="conphoto/90.jpg" class="img-fluid" alt="Responsive image" width="100%">
-
- <div class="container" style="margin-top:30px">
-  <div class="row">
-    <div class="col-sm-8">
-        <div class="card-deck">
-            <div class="card bg-light">
+  <div class="container" style="margin-top:30px">
+    <div class="row">
+      <div class="col-sm-8">
+          <div class="card-deck">
+              <div class="card bg-light">
               <img src="conphoto/89.jpg" class="img-fluid" alt="Responsive image" width="100%">
-              <div class="card-body text-center">
-                <p class="card-text" style="text-align: left;">Some text inside the first card</p>
-                <p class="card-text">Some more text to increase the height</p>
-              </div>
-            </div>
-       
-            <div class="card bg-light">
-              <img src="conphoto/88.jpg" class="img-fluid" alt="Responsive image" width="100%">
                 <div class="card-body text-center">
-                 <p class="card-text" style="text-align: left;">Some text inside the first card</p>
-                 <p class="card-text">Some more text to increase the height</p>
+                  <p class="card-text" style="text-align: left;">Some text inside the first card</p>
+                  <p class="card-text">Some more text to increase the height</p>
+                </div>
+              </div>
+       
+              <div class="card bg-light">
+                <img src="conphoto/88.jpg" class="img-fluid" alt="Responsive image" width="100%">
+                  <div class="card-body text-center">
+                  <p class="card-text" style="text-align: left;">Some text inside the first card</p>
+                  <p class="card-text">Some more text to increase the height</p>
+                  </div>
+              </div>
+          </div><br>
+          <div class="card bg-light">
+              <img src="conphoto/88.jpg" class="img-fluid" alt="Responsive image" width="100%">
+              <div class="card-body text-center">
+                  <p class="card-text" style="text-align: left;">Some text inside the first card</p>
+                  <p class="card-text">Some more text to increase the height</p>
+              </div>
+          </div><br>
+          <div class="card-deck">
+            <div class="card bg-light">
+              <img src="conphoto/91.jpg" class="img-fluid" alt="Responsive image" width="100%">
+              <div class="card-body text-center">
+                  <p class="card-text" style="text-align: left;">Some text inside the first card</p>
+                  <p class="card-text">Some more text to increase the height</p>
+              </div>
+            </div><br>
+            <div class="card bg-light">
+              <img src="conphoto/92.jpg" class="img-fluid" alt="Responsive image" width="100%">
+                <div class="card-body text-center">
+                  <p class="card-text" style="text-align: left;">Some text inside the first card</p>
+                  <p class="card-text">Some more text to increase the height</p>
                 </div>
             </div>
-        
+          </div>
         </div>
-
-        <br>
-        <div class="card bg-light">
-            <img src="conphoto/88.jpg" class="img-fluid" alt="Responsive image" width="100%">
-            <div class="card-body text-center">
-                <p class="card-text" style="text-align: left;">Some text inside the first card</p>
-                <p class="card-text">Some more text to increase the height</p>
+      <div class="col-sm-4">
+        <h2 style="text-align: center">Tendencias y Anuncios</h2>
+        <div id="accordion">
+          <div class="card">
+            <div class="card-header">
+              <a class="card-link" data-toggle="collapse" href="#collapseOne">La ciberseguridad:<br><p style="color: #636363">No solo de tecnologia</p></a>
             </div>
-        </div>
-        
-        <br>
-        <div class="card-deck">
-          <div class="card bg-light">
-            <img src="conphoto/91.jpg" class="img-fluid" alt="Responsive image" width="100%">
-            <div class="card-body text-center">
-                <p class="card-text" style="text-align: left;">Some text inside the first card</p>
-                <p class="card-text">Some more text to increase the height</p>
+            <div id="collapseOne" class="collapse show" data-parent="#accordion">
+              <div class="card-body" style="color: black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </div>
             </div>
           </div>
-            <br>
-          <div class="card bg-light">
-            <img src="conphoto/92.jpg" class="img-fluid" alt="Responsive image" width="100%">
-            <div class="card-body text-center">
-                <p class="card-text" style="text-align: left;">Some text inside the first card</p>
-                <p class="card-text">Some more text to increase the height</p>
+          <div class="card">
+            <div class="card-header">
+              <a class="collapsed card-link" data-toggle="collapse" href="#collapseTwo">NOTICIA 2<br><p style="color: #636363">No solo de tecnologia</p></a>
+            </div>
+            <div id="collapseTwo" class="collapse" data-parent="#accordion">
+              <div class="card-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </div>
             </div>
           </div>
-        
-        </div>
-    </div>
-
-    <div class="col-sm-4">
-      <h2 style="text-align: center">Tendencias y Anuncios</h2>
-   
-  <div id="accordion">
-    <div class="card">
-      <div class="card-header">
-        <a class="card-link" data-toggle="collapse" href="#collapseOne">
-          La ciberseguridad:<br><p style="color: #636363">No solo de tecnologia</p>
-        </a>
-      </div>
-      <div id="collapseOne" class="collapse show" data-parent="#accordion">
-        <div class="card-body" style="color: black">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </div>
-      </div>
-    </div>
-    <div class="card">
-      <div class="card-header">
-        <a class="collapsed card-link" data-toggle="collapse" href="#collapseTwo">
-         NOTICIA 2<br><p style="color: #636363">No solo de tecnologia</p>
-      </a>
-      </div>
-      <div id="collapseTwo" class="collapse" data-parent="#accordion">
-        <div class="card-body">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </div>
-      </div>
-    </div>
-    <div class="card">
-      <div class="card-header">
-        <a class="collapsed card-link" data-toggle="collapse" href="#collapseThree">
-          NOTICIA 3<br><p style="color: #636363">noticia 15062019</p>
-        </a>
-      </div>
-      <div id="collapseThree" class="collapse" data-parent="#accordion">
-        <div class="card-body">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+          <div class="card">
+            <div class="card-header">
+              <a class="collapsed card-link" data-toggle="collapse" href="#collapseThree">NOTICIA 3<br><p style="color: #636363">noticia 15062019</p></a>
+            </div>
+            <div id="collapseThree" class="collapse" data-parent="#accordion">
+              <div class="card-body">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
   </div>
-    </div>
-    
-  </div>
-</div>
 
 </body>
-
-
 
 <!--inicio footer-->
 <?php include 'footer.html'; ?>
