35ef626ff1c95133567a1ab5214d8b60194c9ddb
Gaviota Antonia Herrera Prieto <35613309+GaviotaAntonia@users.noreply.github.com>
2019-06-17 18:08:38 -0500

    Se agrego ifrsmexico

---
+++ ifrsmexico.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Verosu México | Servicios de Auditoría, Impuestos Consultoría, Riesgos y Asesoría Finaciera</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <link rel="shortcut icon" type="image/png" href="conphoto/ab.png"> 
+
+	<link rel="stylesheet" href="css/masestilos.css">
+	<link rel="stylesheet" href="css/estilos.css">
+  <!--LETRA QUE SE UTLIZA-->
+<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+  <style>
+  /* Make the image fully responsive */
+  .carousel-inner img {
+    width: 100%;
+    height: 100%;
+  }
+  #barra{
+    color: black;
+  }
+ .btn-group-vertical>.btn, .btn-group-vertical>.btn-group {
+    width: 188px;
+}
+.contenedor{
+    position: relative;
+    display: inline-block;
+    text-align: center;
+    width: 100%
+}
+ 
+.texto-encima{
+    position: absolute;
+    top: 10px;
+    left: 10px;
+    color: white;
+}
+.centrado{
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    color: White;
+}
+  </style>
+
+</head>
+
+
+<!--NAV -->
+  <?php include 'nav.html'; ?>
+<!--FINAL NAV-->
+<body>
+
+
+
+    <div class="contenedor" align="center">
+        <img class="img-fluid" src="conphoto/85.jpg" width="1800px" style="opacity: 0.4">
+        <div class="centrado"><p style="font-size: 60px"><b>IFRS en México </b><p style="text-align: right;">El equipo de profesionales de Deloitte en IFRS lo ayudará a encontrar la adecuada adopción de las IFRS para su empresa y el cumplimiento de las normativas IFRS en México.</p>
+        </div>
+    </div>
+
+    
+<div class="container" style="margin-top:30px">
+  <div class="row">
+    <div class="col-sm-8">
+        <div class="container mt-3">
+          <h2>Verosu S. de R.L M.I.<small style="color: #76746E"><BR>Servicios Integrales comunicaciones y tecnologia.</small></h2><br><br>
+          <i class='fab fa-facebook-square' style='font-size:48px;color:black'></i>
+          <i class='fab fa-twitter-square' style='font-size:48px;color:black'></i>
+          <i class="fa fa-youtube-square" style="font-size:48px;color:black"></i>
+        </div>
+      </div>
+
+      <div class="col-sm-4">
+          
+                <div class="sub-cuerpo2">
+                    <br><br><a class="twitter-timeline" data-lang="es" data-height="400" data-theme="light" data-link-color="#8C337A" href="https://twitter.com/TwitterDev/timelines/539487832448843776?ref_src=twsrc%5Etfw">National Park Tweets - Curated tweets by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+                </div>
+        </div>
+      </div>
+  </div>
+<hr color="blue" size=10 style="border: 5px">
+
+</body>
+
+<!--inicio footer-->
+<?php include 'footer.html'; ?>
+<!--fin del footer-->
+<br>
+
+</html>
+
