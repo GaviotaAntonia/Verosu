ba4777c6582a4c018210acf3f0f68b70f49bc722
Gaviota Antonia Herrera Prieto <35613309+GaviotaAntonia@users.noreply.github.com>
2019-06-17 13:38:49 -0500

    Modificacion de acerca de a contacto

--- contacto.php
+++ contacto.php
@@ -9,20 +9,109 @@
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   <link rel="shortcut icon" type="image/png" href="conphoto/ab.png"> 
-  
+
   <link rel="stylesheet" href="css/masestilos.css">
   <link rel="stylesheet" href="css/estilos.css">
   <!--LETRA QUE SE UTLIZA-->
 <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
 </head>
+
+
 <!--NAV -->
   <?php include 'nav.html'; ?>
 <!--FINAL NAV-->
 <body>
-	<img src="conphoto/94.jpg" alt="" width="100%" height="200">
-		
-	</div>
-	
+  <!--TARJETAS-->
+   <div class="container" style="margin-top:30px">
+    <div class="col">
+        <div class="card-deck">
+          <div class="card text-white bg-dark">
+              <div class="card-body">          
+                <div class="card-body">
+                  <div align=center>
+                    <i class='fas fa-briefcase' style='font-size:48px;color:white'></i>
+                  </div>
+                    <h4 class="card-title" style="color: white"><br>Trabaja con Verosu</h4>
+                    <p class="card-text" style="color: white">Si buscas empleo o una pasantía en Deloitte México, te invitamos a visitar nuestro sitio de Careers.</p>
+                    <u><a href="#" style="color: white">See Profile</a></u>
+                </div>
+              </div>  
+          </div>
+
+          <div class="card bg-dark">
+              <div class="card-body">
+                  <div class="card-body">
+                    <div align=center>
+                      <i class='fas fa-map-marked-alt' style='font-size:48px;color:white'></i>
+                  </div>
+                    <h4 class="card-title" style="color: white"><br>Nuestras Oficinas</h4>
+                    <p class="card-text" style="color: white">Encuentra la oficina más cercana a tu localidad.</p>
+                    <u><a href="#" style="color: white">Oficinas de Verosu</a></u>
+                  </div>
+              </div>
+          </div>
+
+          <div class="card bg-dark">
+              <div class="card-body">
+                  <div class="card-body">
+                    <div align=center>
+                  <i class='far fa-paper-plane' style="font-size: 48px;color: white"></i>
+                  </div><br>
+                    <h4 class="card-title" style="color: white">Solicitud de cotización</h4>
+                    <p class="card-text" style="color: white">¿Necesitas alguno de nuestros servicios?</p>
+                    <u><a href="#" style="color: white">Envianos tú solicitud</a></u>
+                  </div>
+              </div>
+          </div>
+        </div>
+    </div><br><br>
+  <!---fin tarjetas-->
+
+    <!--APARTADO DONDE VA DIRECCION-->
+      <div class="row">
+        <div class="col-sm-4">
+            <div class="media border p-3">
+            <img src="iconosparapagina/ubicacion.png" alt="John Doe" class="mr-3 mt-3 rounded-circle" style="width:50px;">
+              <div class="media-body">
+                <br><h4>Verosu México<small><br><i></i></small></h4><br><br>
+                <p>Visita la oficina más cercana a tu localidad<br>Ciudad de México<br>Paseo de la Reforma 505<br>Piso 28<br>Colonia Cuauhtémoc<br>Ciudad de México México, 06500<br><a href=""><i class='fas fa-map-marker-alt' style='font-size:24px'></i> Ver mapa </a><br>
+              <a href=""><i class='fas fa-mobile-alt' style='font-size:24px'></i> +52 (55) 5080 6000</a><br>Total of 19 locations</p>
+                <button type="button" class="btn btn-lg btn-outline-info" >
+                  <i class='fas fa-location-arrow'> Ver mas oficinas</i>
+                </button>
+              </div>
+            </div>  
+        </div>
+        <!--fin de apartado de donde va la direccion-->
+
+
+          <!--Registro de contacto en acerda de-->
+          <div class="col-sm-8">            
+            <div class="media-body">
+              <br><h4>Escribenos</h4><br><br>
+              <p>El masivo corte de luz que afectó al país y a ciudades de Brasil y Uruguay fue registrado con extensas notas en los principales diarios y canales de noticias del mundo. La imposibilidad para brindar explicaciones sobre lo sucedido fue lo que más les llamó la atención. "El gobierno no sabe qué lo causó", destacó la CNN.</p><br><p>El apagón que cortó el suministro de energía a todo el país (salvo Tierra del Fuego) y parte de Uruguay y Brasil estuvo registrado con amplias coberturas en los principales diarios y canales de noticias del mundo. <a href="">Las respuestas del Gobierno ante la falla masiva </a>y su imposibilidad para dar certezas sobre lo sucedido, llamaron la atención de esos medios. “Hasta ahora no se ha identificado ninguna explicación para la falla generalizada del suministro eléctrico”, escribió esta mañana la cadena estadounidense CNN en una nota titulada “El blackout dejó a millones en Sudamérica sin energía. El gobierno todavía no sabe qué lo causó”.</p>
+                <form class="form-inline" action="">
+                <label for="email" style="color: black">Nombre(s):</label>
+                <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+                <label for="pwd" style="color: black">Contraseña:</label>
+                <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+                  <div class="form-check">
+                    <label class="form-check-label">
+                      <input class="form-check-input" type="checkbox" name="remember"> Remember me
+                    </label>
+                  </div>
+                  <br><br><button type="submit" class="btn btn-primary">Enviar</button>
+                </form>
+            </div>
+
+          </div>
+
+          <!--FIN DE REGISTRO CONTACTO ACERCA DE -->
+          </div>
+      </div>
+
 
 </body>
 
