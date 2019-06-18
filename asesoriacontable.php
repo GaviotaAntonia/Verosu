79181a97a74d14d0cfd7f2d556194d417fc7ab8b
Gaviota Antonia Herrera Prieto <35613309+GaviotaAntonia@users.noreply.github.com>
2019-06-18 15:16:37 -0500

    Nuevo

--- asesoriacontable.php
+++ asesoriacontable.php
@@ -34,10 +34,12 @@
 		<div class="row">
 
     		<div class="col-sm-1" style="background-color:white;"></div>
-    		<div class="col-sm-3" style="background-color:lavender;">.col-sm-3
+    		<div class="col-sm-3" style="background-color:white;">
 				<div class="container"><br><br>
-  					<div class="card">
-    					<div class="card-body">Basic card</div>
+  					<div class="card" style="border-top: solid 4px #4AA4B9;">
+    					<div class="card-body"><small>Análisis</small><br>Alerta para consejeros 2019
+						<p>Los Consejos de Administración se encuentran en una posición única para supervisar la visión de largo plazo de una organización. Sin embargo, muchos Consejos y equipos ejecutivos a menudo se ven tentados a enfocarse en el corto plazo.</p>
+    					</div>
   					</div><br>
   						<div class="card bg-primary text-white">
     					<div class="card-body">Primary card</div>
@@ -64,10 +66,70 @@
     													<div class="card-body">Light card</div>
   													</div>
 				</div>
-
     		</div>
-    		<div class="col-sm-3" style="background-color:lavenderblush;">.col-sm-3</div>
-    		<div class="col-sm-3" style="background-color:lavender;">.col-sm-3</div>
+
+    		<div class="col-sm-4" style="background-color:white;">
+    				<div class="container" align="center"><br><br>
+  						<div class="card bg-dark text-white" style="border-top: solid 4px #4AA4B9;">
+  							<img src="conphoto/93.jpg" alt="">
+    					<div class="card-body">Dark card</div>
+  						</div><br>
+  						<div class="card bg-primary text-white">
+    					<div class="card-body">Primary card</div>
+  						</div><br>
+  							<div class="card bg-success text-white">
+    							<div class="card-body">Success card</div>
+  							</div><br>
+  								<div class="card bg-info text-white">
+    								<div class="card-body">Info card</div>
+  								</div><br>
+  									<div class="card bg-warning text-white">
+    									<div class="card-body">Warning card</div>
+  									</div><br>
+  										<div class="card bg-danger text-white">
+    										<div class="card-body">Danger card</div>
+  										</div><br>
+  											<div class="card bg-secondary text-white">
+    											<div class="card-body">Secondary card</div>
+  											</div><br>
+  												<div class="card bg-dark text-white">
+    												<div class="card-body">Dark card</div>
+  												</div><br>
+  													<div class="card bg-light text-dark">
+    													<div class="card-body">Light card</div>
+  													</div>
+					</div>
+    		</div>
+
+    		<div class="col-sm-2" style="background-color:white;">
+    				<div class="container"><br><br>
+  					
+  						<div class="card bg-primary text-white" style="border-top: solid 4px #4AA4B9;">
+    					<div class="card-body">Primary card</div>
+  						</div><br>
+  							<div class="card bg-success text-white">
+    							<div class="card-body">Success card</div>
+  							</div><br>
+  								<div class="card bg-info text-white">
+    								<div class="card-body">Info card</div>
+  								</div><br>
+  									<div class="card bg-warning text-white">
+    									<div class="card-body">Warning card</div>
+  									</div><br>
+  										<div class="card bg-danger text-white">
+    										<div class="card-body">Danger card</div>
+  										</div><br>
+  											<div class="card bg-secondary text-white">
+    											<div class="card-body">Secondary card</div>
+  											</div><br>
+  												<div class="card bg-dark text-white">
+    												<div class="card-body">Dark card</div>
+  												</div><br>
+  													<div class="card bg-light text-dark">
+    													<div class="card-body">Light card</div>
+  													</div>
+				</div>
+    		</div>
 
     		   <div class="col-sm-2">
                 <div class="sub-cuerpo2">
@@ -76,16 +138,16 @@
                 <br><br>
                 <div class="container">
                   <div class="list-group">
-                    <a href="contacto.php" class="list-group-item list-group-item-action" style="background-color: #3E473F; color: gray;"><b>Contacto</b><br>
+                    <a href="contacto.php" class="list-group-item list-group-item-action" style="background-color: #59C3CC; color: white;"><b>Contacto</b><br>
                       <button type="" class="btn btn-light btn-sm btn-block">Escribenos a traves de nuestra solicitud en línea</button></a> 
                     <br>
-                    <a href="#" class="list-group-item list-group-item-action" style="background-color: #3E473F; color: gray;"><b>Solicitud de cotización</b><br><button type="" class="btn btn-light btn-sm btn-block">Enviar Formulario electronico</button></a> 
+                    <a href="#" class="list-group-item list-group-item-action" style="background-color: #59C3CC; color: white;"><b>Solicitud de cotización</b><br><button type="" class="btn btn-light btn-sm btn-block">Enviar Formulario electronico</button></a> 
                    
                     <br>
-                    <a href="#" class="list-group-item list-group-item-action" style="background-color: #3E473F; color: gray;"><b>Servicios y Soluciones</b></a>
+                    <a href="#" class="list-group-item list-group-item-action" style="background-color: #59C3CC; color: white;"><b>Servicios y Soluciones</b></a>
                     <br>
                         <div class="card">
-                          <div class="card-header" style="background-color: #3E473F; color: white"><b>Temas Similares</b></div>
+                          <div class="card-header" style="background-color: #59C3CC; color: white"><b>Temas Similares</b></div>
                           <div class="card-body">
                             <a href="enbreveifrs.php">En Breve IFRS</a><br>
                             <a href="asesoriacontable.php">Asesoria Contable</a>
