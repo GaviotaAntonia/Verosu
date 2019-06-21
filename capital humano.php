96f7502f2f4db62a2c65868195e5e14e17d94cd1
EnriqueOrozco <51837209+EnriqueOrozco@users.noreply.github.com>
2019-06-21 10:29:44 -0500

    Capital Humano

--- capital humano.php
+++ capital humano.php
@@ -1,194 +1,243 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>VEROSU S. de R.L M.I</title>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-  <link rel="shortcut icon" type="image/png" href="conphoto/ab.png"> 
+  <head>
+    
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>VEROSU S. de R.L M.I</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <link rel="shortcut icon" type="image/png" href="conphoto/ab.png"> 
 
-	<link rel="stylesheet" href="css/masestilos.css">
-	<link rel="stylesheet" href="css/estilos.css">
-  <!--LETRA QUE SE UTLIZA-->
-<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-  <style>
-  /* Make the image fully responsive */
-  .carousel-inner img {
-    width: 100%;
-    height: 100%;
-  }
-  #barra{
-    color: black;
-  }
- .btn-group-vertical>.btn, .btn-group-vertical>.btn-group {
-    width: 188px;
-}
-.contenedor{
-    position: relative;
-    display: inline-block;
-    text-align: center;
-    width: 100%
-}
- 
-.texto-encima{
-    position: absolute;
-    top: 10px;
-    left: 10px;
-    color: white;
-}
-.centrado{
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-
-    color: White;
-}
-  </style>
-
-</head>
-
-<body>
-	<!--NAV1-->
-	<?php include "nav.html"; ?>
-  <!--nav 2-->
-  <?php include 'nav2.html'; ?>
-<!---FIN DE NAV BAR-->
+    <link rel="stylesheet" href="css/masestilos.css">
+    <link rel="stylesheet" href="css/estilos.css">
   
-      <div class="contenedor" align="center">
-        <img class="img-fluid" src="conphoto/105.jpg" width="1800px" style="opacity: 0.4">
-        <div class="centrado">
-          <p style="font-size: 60px; color: black;">
-            <b>Capital Humano</b>
-          <p style="text-align: right; color: black;">Acompañaremos a nuestros clientes a resolver sus retos organizaciones más complejos, por medio de nuestras soluciones en materia de talento, organización y cambio que ligan la estrategia de negocio con la de Recursos Humanos.</p>
-        </div>
+    <!--LETRA QUE SE UTLIZA-->
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+
+    <style>
+      
+      /* Make the image fully responsive */
+      
+      .carousel-inner img 
+      {
+        width: 100%;
+        height: 100%;
+      }
+    
+      #barra
+      {
+        color: black;
+      }
+ 
+      .btn-group-vertical>.btn, .btn-group-vertical>.btn-group 
+      {
+        width: 188px;
+      }
+    
+      .contenedor
+      {
+        position: relative;
+        display: inline-block;
+        text-align: center;
+        width: 100%
+      }
+ 
+      .texto-encima
+      {
+        position: absolute;
+        top: 10px;
+        left: 10px;
+        color: white;
+      }
+
+      .centrado
+      {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: White;
+      }
+    </style>
+
+  </head>
+
+  <body>
+
+  <?php include "nav.html" ?>
+  <?php include "nav2.html" ?>
+
+  <div class="contenedor" align="center">
+    <img class="img-fluid" src="conphoto/105.jpg" width="1800px" style="opacity: 0.4">
+    <div class="centrado">
+      <p style="font-size: 60px; color: black">
+        <b>Capital Humano</b>
+      </p>
+      <p style="text-align: right; color: black">
+        Acompañaremos a nuetros clientes a resolver sus retos organizaciones más complejos, por medio de nuestras soluciones en materia de talento, organización y cambio que ligan la estrategia de negocio con la de Recursos Humanos
+      </p>
     </div>
-      <div class="container" style="margin-top:30px">
-      <div class="row">
-        <div class="col-sm-8">
+  </div>
+  <div class="container" style="margin-top: 30px">
+    <div class="row">
+      <div class="col-sm-8">
         <div class="container mt-3">
-          <h2>Verosu S. de R.L M.I.<small style="color: #76746E"><BR>Servicios Integrales comunicaciones y tecnologia.</small></h2><br><br>
-          <i class='fab fa-facebook-square' style='font-size:48px;color:black'></i>
-          <i class='fab fa-twitter-square' style='font-size:48px;color:black'></i>
-          <i class="fa fa-youtube-square" style="font-size:48px;color:black"></i>
-        </div><br><br>
-
-
-        <!--- 
-        <div class="bs-example">
-          <div class="card" style="max-width: 730px;">
+          <h2>
+            VEROSU S. de R.L M.I
+            <br>
+            <small style="color: #76746E">
+              Servicios Integrales Comunicaciones y Tecnología
+            </small>
+          </h2>
+          <br>
+          <i class="fab fa-facebook-square" style="font-size: 48px; color: black"></i>
+          <i class="fab fa-twitter-square" style="font-size: 48px; color: black"></i>
+          <i class="fa fa-youtube-square" style="font-size: 48px; color: black"></i>
+        </div> 
+        <br>
+        <div class="bs-example"> 
+          <div class="card" style="max-width: 730px"> 
             <div class="row no-gutters">
-              <div class="col-md-4" style="background: #868e96;">
+              <div class="col-md-4" style="background-color: #868e96">
                 <img src="conphoto/1.jpeg" class="card-img-top h-100" alt="...">
               </div>
               <div class="col-md-7">
-                  <div class="card-body">
-                    <b><p style="color: gray">Noticias </p></b>
-                      <h5 class="card-title">IFRS 9</h5>
-                      <p class="card-text" style="color: black">Retos y oportunidades en su implementación.</p>
-                      <a href="#" class="btn btn-light">Conozca más</a>
-                  </div>
-              </div>
-            </div>
-        </div>
-        </div>
-        <!---Fin Articulos 1-->
-        <br><br>
-          <div class="card-deck">
-            <div class="card text-white bg-dark">
-              <img src="conphoto/97.jpg" height="50" alt="">
-              <div class="card-body text-center">
-                <p class="card-text">Some text inside the first card</p>
-              </div>
-            </div>
-              <div class="card text-white bg-dark" id="2">
-                <hr style="color: #0056b2;">
                 <div class="card-body">
-                  <p style="color: white"><b>Servicios</b></p>
-                  <h3 style="color: White">Cursos y Diplomados en IFRS</h3>
-                  <p class="card-text">Some text inside the second card</p>
+                  <p style="color: gray">
+                    <b>Noticias</b>
+                  </p>
+                  <h5 class="card-title">IFRS 9</h5>
+                  <p class="card-text" style="color: black">Retos y Oportunidades en su implementación</p>
+                  <a href="#" class="btn btn-light">Conozca más</a>
                 </div>
-
-              </div>
-                <div class="card text-white bg-dark">
-                  <img src="imgenes/financiero.jpg" alt="" height="50">
-                    <div class="card-body">
-                      <p style="color: white"><b>Servicios</b></p>
-                      <h3 style="color: White">Cursos y Diplomados en IFRS</h3>
-                      <p class="card-text">Some text inside the second card</p>
-                    </div>
-                </div>
-          </div>
-          <br><br>
-          <div class="card-deck">
-            <div class="card text-white bg-dark">
-              <img src="conphoto/97.jpg" height="50" alt="">
-              <div class="card-body text-center">
-                <p class="card-text">Some text inside the first card</p>
               </div>
             </div>
-        
-                <div class="card text-white bg-dark">
-                  <img src="imgenes/financiero.jpg" alt="" height="50">
-                    <div class="card-body">
-                      <p style="color: white"><b>Servicios</b></p>
-                      <h3 style="color: White">Cursos y Diplomados en IFRS</h3>
-                      <p class="card-text">Some text inside the second card</p>
-                    </div>
-                </div>
           </div>
+        </div>
+        <br>
+        <!--Cambiar texto --->
+        <div class="card-deck">
+            <div class="card text-white ">
+              <img src="conphoto/97.jpg" height="50">
+              <div class="card-body">
+                <p style="color: #97999b">
+                  <b>Análisis</b>
+                </p>
+                <p style="color: black">Estudio "Madurez en los procesos financieros"</p>
+                <p style="color: #97999b">Conozca los resultados de nuestro estudio Madurez...</p>
+                <p style="color: black">
+                  <b>Leer más</b>
+                </p>
+              </div>
+            </div>
+            <div class="card text-white" style="background: #000; border-top: solid 5px #ff9786">
+              <div class="card-body">
+                <p style="color: #97999b">
+                  <b>Artículo</b>
+                </p>
+                <p style="color: white">Diplomados IFRS</p>
+                <p style="color: #97999b">En este curso podrá adquirir el conocimiento de...</p>
+                <p style="color: white">
+                  <b>Insríbase aquí</b>
+                </p>
+              </div>
+            </div>
+            <div class="card text-white ">
+              <div class="card-body">
+                <p style="color: #97999b">
+                  <b>Artículo</b>
+                </p>
+                <p style="color: black">Cursos y Diplomados en IFRS</p>
+                <p style="color: #97999b">Alianzas con Instituciones Universitarias</p>
+                <p style="color: black">
+                  <b>Descubra más</b>
+                </p>
+              </div>
+            </div>
+        </div>
+        <br><br>
+        <div class="card-deck">
+            <div class="card text-white" style="background: #000">
+              <img src="conphoto/97.jpg" height="50">
+              <div class="card-body text-center">
+                <p style="color: #fff">Some text inside the first card</p>
+              </div>
+            </div>
+            <div class="card text-white" style="background: #000; border-top: solid 5px #00a3e0">
+              <div class="card-body">
+                <p style="color: #97999b">
+                  <b>Servicios</b>
+                </p>
+                <p style="color: white">Cursos y Diplomados en IFRS</p>
+                <p style="color: #97999b">Verosu ofrece cursos y diplomados en IFRS en conjunto con Instituciones Universitarias para apoyar a las organizaciones en su proceso de preparación y actualización en las IFRS.</p>
+              </div>
+            </div>
+        </div>
+        <!--Cambiar texto --->
       </div>
-
-
-
       <div class="col-sm-4">
-                            
-                <div class="container">
-                  <div class="list-group">
-
-                    <div class="container">
-                      <br>
-                      <div class="list-group">
-                        <div class="card-header" style="background: black"><p style="color: white; ">Servicios y Soluciones</p></div>
-                        <a href="#" class="list-group-item list-group-item-action">First item</a>
-                          <a href="#" class="list-group-item list-group-item-action">Second item</a>
-                            <a href="#" class="list-group-item list-group-item-action">Third item</a>
-                            <a href="#" class="list-group-item list-group-item-action">cuatro itme</a>
-                      </div>
-                    </div> <br>
-                    <a href="contacto.php" class="list-group-item list-group-item-action" style="background-color: #3E473F; color: gray;"><b>Contacto</b><br>
-                      <button type="" class="btn btn-light btn-sm btn-block">Escribenos a traves de nuestra solicitud en línea</button></a> 
-                    <br>
-                    <a href="#" class="list-group-item list-group-item-action" style="background-color: #3E473F; color: gray;"><b>Solicitud de cotización</b><br><button type="" class="btn btn-light btn-sm btn-block">Enviar Formulario electronico</button></a> 
-                   
-                    <br>
-                    <a href="#" class="list-group-item list-group-item-action" style="background-color: #3E473F; color: gray;"><b>Servicios y Soluciones</b></a>
-                    <br>
-                        <div class="card">
-                          <div class="card-header" style="background-color: #3E473F; color: white"><b>Temas Similares</b></div>
-                          <div class="card-body">
-                            <a href="enbreveifrs.php">En Breve IFRS</a><br>
-                            <a href="asesoriacontable.php">Asesoria Contable</a>
-                            <br><a href="auditoriadeestadosfinancieros.php">Auditoria de Estados Financieros</a>
-                            <br><a href="finanzascorporativas.php">Finanzas Corporativas</a>
-                            <br><a href="auditoriainterna.php">Auditoria interna</a>
-                          </div> 
-                        </div>
-                    </div>
-                     <div class="sub-cuerpo2">
-                    <br><br><a class="twitter-timeline" data-lang="es" data-height="400" data-theme="light" data-link-color="#8C337A" href="https://twitter.com/TwitterDev/timelines/539487832448843776?ref_src=twsrc%5Etfw">National Park Tweets - Curated tweets by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-                </div>
-
+        <div class="container">
+          <div class="sub-cuerpo2">
+            <br>
+            <a class="twitter-timeline" data-lang="es" data-height="400" data-theme="light" data-link-color="#8C337A" href="https://twitter.com/TwitterDev/timelines/539487832448843776?ref_src=twsrc%5Etfw">National Park Tweets - Curated tweets by TwitterDev</a> 
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            <br>
+            <div class="container">
+              <div class="list-group">
+                <a href="contacto.php" class="list-group-item list-group-item-action" style="background-color: #59C3CC; color: white">
+                  <b>Contacto</b>
+                  <button type="" class="btn btn-light btn-sm btn-block">Escribenos a traves de nuestra solicitud en línea</button>
+                </a>
+                <br>
+                <a href="#" class="list-group-item list-group-item-action" style="background-color: #59C3CC; color: white">
+                  <b>Solicitud de cotización</b>
+                  <button type="" class="btn btn-light btn-sm btn-block">Enviar formulario electronico</button>
+                </a>
+                <br>
+                <div class="list-group">
+                  <div class="card-header" style="background: #59C3CC">
+                    <p style="color: white">
+                      <b>Servicios y Soluciones</b>
+                    </p>
                   </div>
-      </div>      </div>
+                  <a href="#" class="list-group-item list-group-item-action">First item</a>
+                  <a href="#" class="list-group-item list-group-item-action">Second item</a>
+                  <a href="#" class="list-group-item list-group-item-action">Third item</a>
+                  <a href="#" class="list-group-item list-group-item-action">cuatro itme</a>
+                </div>
+                <br>
+                <div class="card">
+                  <div class="card-header" style="background-color: #59C3CC; color: white">
+                    <b>Temas Similares</b>
+                  </div>
+                  <div class="card-body">
+                    <a href="#">1</a>
+                    <br>
+                    <a href="#">2</a>
+                    <br>
+                    <a href="#">3</a>
+                    <br>
+                    <a href="#">4</a>
+                    <br>
+                    <a href="#">5</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 
-	<?php include "footer.html"; ?>
+  <?php include "footer.html" ?>
 
-</body>
+  </body>
+
 </html>
+
