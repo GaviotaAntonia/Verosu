f14944824e6b1b6b1da32cb8022e128c7f95685c
EnriqueOrozco <51837209+EnriqueOrozco@users.noreply.github.com>
2019-06-22 13:02:09 -0500

    Nuevos avances

--- riesgo financiero.php
+++ riesgo financiero.php
@@ -11,89 +11,198 @@
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   	<link rel="shortcut icon" type="image/png" href="conphoto/ab.png"> 
 
-	<link rel="stylesheet" href="css/masestilos.css">
-	<link rel="stylesheet" href="css/estilos.css">
-  <!--LETRA QUE SE UTLIZA-->
-<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-  <style>
-  /* Make the image fully responsive */
-  .carousel-inner img {
-    width: 100%;
-    height: 100%;
-  }
-  #barra{
-    color: black;
-  }
- .btn-group-vertical>.btn, .btn-group-vertical>.btn-group {
-    width: 188px;
-}
-.contenedor{
-    position: relative;
-    display: inline-block;
-    text-align: center;
-    width: 100%
-}
+	  <link rel="stylesheet" href="css/masestilos.css">
+	  <link rel="stylesheet" href="css/estilos.css">
+  
+    <!--LETRA QUE SE UTLIZA-->
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+  
+    <style>
+      /* Make the image fully responsive */
+      
+      .carousel-inner img 
+      {
+        width: 100%;
+        height: 100%;
+      }
+
+      #barra
+      {
+        color: black;
+      }
  
-.texto-encima{
-    position: absolute;
-    top: 10px;
-    left: 10px;
-    color: white;
-}
-.centrado{
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+      .btn-group-vertical>.btn, .btn-group-vertical>.btn-group 
+      {
+        width: 188px;
+      }
 
-    color: White;
-}
-  </style>
+      .contenedor
+      {
+        position: relative;
+        display: inline-block;
+        text-align: center;
+        width: 100%
+      }
+ 
+      .texto-encima
+      {
+        position: absolute;
+        top: 10px;
+        left: 10px;
+        color: white;
+      }
 
-</head>
-<body>
-  <!--NAV1-->
-  <?php include "nav.html"; ?>
-  <!--nav 2-->
-  <?php include 'nav2.html'; ?>
-<!---FIN DE NAV BAR-->
+      .centrado
+      {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: White;
+      }
+  
+    </style>
 
+  </head>
+  <body>
+  
+    <!--NAV1-->
+      <?php include "nav.html"; ?>
+    <!--nav 2-->
+      <?php include 'nav2.html'; ?>
+    <!---FIN DE NAV BAR-->
 
-
-
-	<div class="contenedor" align="center">
-        <img class="img-fluid" src="conphoto/108.jpg" width="1800px" style="opacity: 0.4">
-        <div class="centrado">
-        	<p style="font-size: 60px; color: black;">
-        		<b>Riesgo Financiero</b>
-        	<p style="text-align: right; color: black;">Ayudamos a las organizaciones a administrar sus áreas de riesgo financiero mediante la asistencia en el diseño y la implementación de gobierno, procesos, modelos, datos, tecnología y rendimiento de informes, incluyendo soporte durante todo el ciclo de vida de las operaciones</p>
-        </div>
+	  <div class="contenedor" align="center">
+      <img class="img-fluid" src="conphoto/108.jpg" width="1800px" style="opacity: 0.4">
+      <div class="centrado">
+        <p style="font-size: 60px; color: black;">
+        	<b>Riesgo Financiero</b>
+        <p style="text-align: right; color: black;">Ayudamos a las organizaciones a administrar sus áreas de riesgo financiero mediante la asistencia en el diseño y la implementación de gobierno, procesos, modelos, datos, tecnología y rendimiento de informes, incluyendo soporte durante todo el ciclo de vida de las operaciones</p>
+      </div>
     </div>
-  <?php include 'navriesgo.html'; ?>
+  
+    <?php include 'navriesgo.html'; ?>
+    
     <div class="container" style="margin-top: 30px">
     	<div class="row">
-			<div class="col-sm-8">
-				<div class="container mt-3">
-					<h2>
-						Verosu S. de R.L M.I
-						<small style="color: #76746E">
-							<br>
-							Servicios Integrales Comunicaciones y Tecnologia.
-						</small>
-					</h2>
-					<br><br>
-					<i class="fab fa-facebook-square" style="font-size: 48px; color: black"></i>
-					<i class="fab fa-twitter-square" style="font-size: 48px; color: black"></i>
-					<i class="fa fa-youtube-square" style="font-size: 48px; color: black"></i>
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="sub-cuerpo2">
-					<br><br>
-					<a class="twitter-timeline" data-lang="es" data-height="400" data-theme="light" data-link-color="#8C337A" href="https://twitter.com/TwitterDev/timelines/539487832448843776?ref_src=twsrc%5Etfw">National Park Tweets - Curated tweets by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-				</div>
-			</div>    		
+			  <div class="col-sm-8">
+				  <div class="container mt-3">
+					  <h2>
+						  Verosu S. de R.L M.I
+						  <small style="color: #76746E">
+							  <br>
+							  Servicios Integrales Comunicaciones y Tecnologia.
+						  </small>
+					  </h2>
+					  <br><br>
+					  <i class="fab fa-facebook-square" style="font-size: 48px; color: black"></i>
+					  <i class="fab fa-twitter-square" style="font-size: 48px; color: black"></i>
+					  <i class="fa fa-youtube-square" style="font-size: 48px; color: black"></i>
+				  </div>
+          <br><br>
+          <div class="bs-example">
+            <div class="card" style="max-width: 730px;">
+              <div class="row no-gutters">
+                <div class="col-md-8" style="background: #000">
+                  <div class="card-body">
+                    <b>
+                      <p style="color: gray">Servicios</p>
+                    </b>
+                    <h5 class="card-title" style="color: white">Riesgos Financieros</h5>
+                    <p class="card-text" style="color: white">Nuestros servicios de amplio alcance ayudan a las entidades en la identificacion, evaluacion, modelacion, comunicacion y limitacion de riesgos financieros</p>
+                    <a href="#" class="btn btn-light">Descargar infografia</a>
+                  </div>
+                </div>
+                <div class="col-md-4" style="background: #868e96;">
+                  <img src="conphoto/1.jpeg" class="card-img-top h-100" alt="...">
+                </div>
+              </div>
+            </div>
+          </div>
+          <br><br>
+          <div class="bs-example">
+            <div class="card" style="max-width: 730px;">
+              <div class="row no-gutters">
+                <div class="col-md-8" style="background: #fff">
+                  <div class="card-body">
+                    <b>
+                      <p style="color: gray">Articulo</p>
+                    </b>
+                    <h5 class="card-title" style="color: black">Cambios regulatorios en el sector asegurador y afianzador mexicano</h5>
+                    <p class="card-text" style="color: black">Vision estrategica de la nueva Ley de Instituciones de Seguros y de Fianzas</p>
+                    <a href="#" class="btn btn-light">Leer más</a>
+                  </div>
+                </div>
+                <div class="col-md-4" style="background: #868e96;">
+                  <img src="conphoto/1.jpeg" class="card-img-top h-100" alt="...">
+                </div>
+              </div>
+            </div>
+          </div>
+          <br><br>
+          <div class="bs-example">
+            <div class="card" style="max-width: 730px;">
+              <div class="row no-gutters">
+                <div class="col-md-8" style="background: #000">
+                  <div class="card-body">
+                    <b>
+                      <p style="color: gray">Perspectivas</p>
+                    </b>
+                    <h5 class="card-title" style="color: white">Desmitificando la cobertura de los ciberseguros</h5>
+                    <p class="card-text" style="color: white">A pesar de que cada vez son mas los recursos que las empresas destinan en materia de ciberseguridad para protegerse a si mismas, las inversiones que estas realizan para contratar ciberseguros siguen siendo muy escasas</p>
+                    <a href="#" class="btn btn-light">Leer más</a>
+                  </div>
+                </div>
+                <div class="col-md-4" style="background: #868e96;">
+                  <img src="conphoto/1.jpeg" class="card-img-top h-100" alt="...">
+                </div>
+              </div>
+            </div>
+          </div>
+			  </div>
+			  <div class="col-sm-4">
+				  <div class="sub-cuerpo2">
+					  <br><br>
+					  <a class="twitter-timeline" data-lang="es" data-height="400" data-theme="light" data-link-color="#8C337A" href="https://twitter.com/TwitterDev/timelines/539487832448843776?ref_src=twsrc%5Etfw">National Park Tweets - Curated tweets by TwitterDev</a> 
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+				  </div>
+          <br>
+          <div class="container">
+            <div class="list-group">
+              <a href="contacto.php" class="list-group-item list-group-item-action" style="background-color: #59C3CC; color: white">
+                <b>Contacto</b>
+                <button type="" class="btn btn-light btn-sm btn-block">Escribenos a traves de nuestra solicitud en línea</button>
+              </a>
+              <br>
+              <a href="#" class="list-group-item list-group-item-action" style="background-color: #59C3CC; color: white">
+                <b>Solicitud de cotización</b>
+                <button type="" class="btn btn-light btn-sm btn-block">Enviar formulario electronico</button>
+              </a>
+              <br>
+              <a href="" class="list-group-item list-group-item-action" style="background-color: #59C3CC; color: white">
+                <b>Servicios</b>
+              </a>
+              <br>
+              <div class="card">
+                <div class="card-header" style="background-color: #59C3CC; color: white">
+                  <b>Temas Similares</b>
+                </div>
+                <div class="card-body">
+                  <a href="#">1</a>
+                  <br>
+                  <a href="#">2</a>
+                  <br>
+                  <a href="#">3</a>
+                  <br>
+                  <a href="#">4</a>
+                  <br>
+                  <a href="#">5</a>
+                </div>
+              </div>
+            </div>
+          </div>
+			  </div>    		
     	</div>
     </div>
 
