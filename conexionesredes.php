c93e1f31b119ec65a0057462c1b6a5f67fd7c379
Gaviota Antonia Herrera Prieto <35613309+GaviotaAntonia@users.noreply.github.com>
2019-06-24 17:36:01 -0500

    NUEVOS CAMBIOS

--- conexionesredes.php
+++ conexionesredes.php
@@ -74,11 +74,11 @@
     <!---FIN DE NAV BAR-->
 
 	    <div class="contenedor" align="center">
-        <img class="img-fluid" src="conphoto/routerswi.jpg" width="1800px" style="opacity: 0.4">
+        <img class="img-fluid" src="conphoto/redes.jpg" width="1800px" style="opacity: 0.4">
         <div class="centrado">
         	<p style="font-size: 60px; color: black;">
-        		<b>Configuración de Switch y Router</b>
-        	<p style="text-align: right; color: black;">Los switches domésticos para un uso básico de red no necesitan una configuración compleja más allá de la instalación cableada básica y su alimentación.</p>
+        		<b>Redes</b>
+        	<p style="text-align: right; color: black;">Se entiende por redes informáticas, redes de comunicaciones de datos o redes de computadoras a un número de sistemas informáticos conectados entre sí mediante una serie de dispositivos alámbricos o inalámbricos, gracias a los cuales pueden compartir información en paquetes de datos, transmitidos mediante impulsos eléctricos, ondas electromagnéticas o cualquier otro medio físico.<br>Fuente:<a href="https://concepto.de/redes-informaticas/#ixzz5rnhQKZl9">https://concepto.de/redes-informaticas/#ixzz5rnhQKZl9</a></p>
         </div>
       </div>
       <div class="container" style="margin-top: 30px">
@@ -90,15 +90,15 @@
                   <div class="col-md-8" style="background: #000">
                     <div class="card-body">
                       <b>
-                        <p style="color: gray">Articulo</p>
+                        <p style="color: gray">Noticias</p>
                       </b>
-                      <h5 class="card-title" style="color: white">¿Qué es Routing y Switching?</h5>
-                      <p class="card-text" style="color: white">El funcionamiento de una red consiste en conectar computadoras y periféricos mediante dos partes del equipo: switches y routers. Estos dos elementos permiten a los dispositivos conectados a la red comunicarse con los demás y con otras redes.</p>
+                      <h5 class="card-title" style="color: white">¿Que son las redes?</h5>
+                      <p class="card-text" style="color: white">Te explicamos qué son las redes informáticas y cuáles son los tipos que existen. Además, sus elementos y qué es una topología de red.</p>
                       <a href="https://www.ecured.cu/Redes_Inform%C3%A1ticas" class="btn btn-light">Leer más</a>
                     </div>
                   </div>
                   <div class="col-md-4" style="background: #868e96;">
-                    <img src="conphoto/switrout.jpg" class="card-img-top h-100" alt="...">
+                    <img src="conphoto/redes1.jpg" class="card-img-top h-100" alt="...">
                   </div>
                 </div>
               </div>
@@ -110,20 +110,20 @@
                   <p style="color: #97999b">
                     <b>Noticias</b>
                   </p>
-                  <p style="color: black">Información.</p>
-                  <p style="color: #97999b">Administración de redes.</p>
-                  <p style="color: black">Mejore la eficacia operativa y reduzca las interrupciones de la red con los mejores productos y soluciones de su clase para administrar una red empresarial.
-                    <a href="https://www.cisco.com/c/es_mx/support/docs/ip/routing-information-protocol-rip/13719-50.html"><b>Leer más</b></a>
+                  <p style="color: black">Con instructor.</p>
+                  <p style="color: #97999b">CCNA R&S: Connecting Networks<br>El cuarto y último curso de esta serie lo prepara para la certificación Cisco CCNA R&S.</p>
+                  <p style="color: black">
+                    <a href="https://www.proydesa.org/portal/index.php/carreras/redes/ccna-4"><b>Leer más</b></a>
                   </p>
                 </div>
               </div>
               <div class="card text-white" style="border-top: solid 4px #009975">
                 <div class="card-body">
                   <p style="color: #97999b">
-                    <b>Articulos</b>
+                    <b>Noticias</b>
                   </p>
-                  <p style="color: black">Cisco Networking: Mostrar comando de versión en un switch</p>
-                  <p style="color: #97999b">En cualquier componente de la red de Cisco, el comando show version es aplicable a cualquier hardware que utilice el Sistema Operativo Internetwork (IOS) de Cisco, incluidos los switches.</p>
+                  <p style="color: black">Cumplimiento de la Norma IFFRS16</p>
+                  <p style="color: #97999b">Nueva solución desarrollada por Verosu, plataforma SAP S/4HANA Real Estate</p>
                   <p style="color: black">
                     <a href="https://www.akamai.com/es/es/resources/security-compliance.jsp"><b>Conoce más</b></a>
                   </p>
@@ -132,17 +132,83 @@
               <div class="card text-white" style="border-top: solid 4px #009975">
                 <div class="card-body">
                   <p style="color: #97999b">
-                    <b>Informacion</b>
+                    <b>Con instructor</b>
                   </p>
-                  <p style="color: black">Nuevos Routers</p>
-                  <p style="color: #97999b">Impulsados por la intención y con información del contexto y seguridad cabal integrada. Nuestros switches ayudan a su red a aprender y evolucionar constantemente para cumplir las necesidades de su negocio.</p>
+                  <p style="color: black">CCNA R&S: Scaling Networks</p>
+                  <p style="color: #97999b">El tercer y penúltimo curso de la serie lo prepara para obtener empleos como ingeniero y analista</p>
                   <p style="color: black">
-                    <a href="https://www.cisco.com/c/es_mx/products/switches/index.html"><b>Conoce más</b></a>
+                    <a href="https://www.netacad.com/es/courses/networking/networking-essentials"><b>Conoce más</b></a>
                   </p>
                 </div>
               </div>
             </div>
-         <br>
+            <br><br>
+            <div class="card-deck">
+              <div class="card text-white " style="border-top: solid 4px #009975">
+                <div class="card-body">
+                  <p style="color: #97999b">
+                    <b>Noticias</b>
+                  </p>
+                  <p style="color: black">EEUU Lanza ciberataque contra Íran</p>
+                  <p style="color: #97999b">La operación fue una recomendación del Pentágono para deteriorar el funcionamiento de la red que controla los misiles iraníes y para reducir las capacidades ofensivas informáticas de la Guardia Revolucionaria Islámica.</p>
+                  <p style="color: black">
+                    <a href="http://www.redusers.com/noticias/eeuu-lanzo-ciberataque-iran/"><b>Leer más</b></a>
+                  </p>
+                </div>
+              </div>
+              <div class="card text-white" style="border-top: solid 4px #009975">
+                <div class="card-body">
+                  <p style="color: #97999b">
+                    <b>Noticias</b>
+                  </p>
+                  <p style="color: black">La comunidad andaluza, entre las tres peor protegidas ante ataques informáticos</p>
+                  <p style="color: #97999b">En el primer trimestre se detectaron 130.000 problemas de seguridad que afectan a empresas y ciudadanos</p>
+                  <p style="color: black">
+                  <a href="https://www.larazon.es/local/andalucia/la-comunidad-andaluza-entre-las-tres-peor-protegidas-ante-ataques-informaticos-EI23900613"><b>Conoce más</b></a>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <br><br>
+            <div class="card-deck">
+              <div class="card text-white" style="border-top: solid 4px #009975">
+                <div class="card-body">
+                  <p style="color: #97999b">
+                    <b>Con Instructor</b>
+                  </p>
+                  <p style="color: black">9 nuevas profesiones que ganan 20% más que las tradicionales.</p>
+                  <p style="color: #97999b">Durante la presentación del análisis de Nuevas Profesiones Digitales 2019 del Instituto Superior de Desarrollo de Internet (ISDI), se aseguró que a medida que la economía digital evoluciona, las empresas requieren perfiles digitales con un alto grado de especialización, mismos que alcanzan ingresos 20% superiores a la media y en algunos casos más.</p>
+                  <p style="color: black">
+                    <a href="https://www.dineroenimagen.com/management/9-nuevas-profesiones-que-ganan-20-mas-que-las-tradicionales/111225"><b>Conozca más</b></a>
+                  </p>
+                </div>
+              </div>
+              <div class="card text-white" style="border-top: solid 4px #009975">
+                <div class="card-body">
+                  <p style="color: #97999b">
+                    <b>Noticias</b>
+                  </p>
+                  <p style="color: black">Más del 10% de los delitos en el Bierzo vienen por redes informáticas</p>
+                  <p style="color: #97999b">El capitán de la Comandancia de la Guardia Civil en Ponferrada, José Julio Martín alertó de la escalada de los delitos que utilizan los medios cibernéticos para llevarse a cabo en las jornadas sobre mediación familiar organizadas por la Policía Municipal de Ponferrada a lo largo de estos días en la Uned. </p>
+                  <p style="color: black">
+                    <a href="https://www.lanuevacronica.com/mas-del-10-de-los-delitos-en-el-bierzo-vienen-por-redes-informaticas"><b>Conoce más</b></a>
+                  </p>
+                </div>
+              </div>
+              <div class="card text-white" style="border-top: solid 4px #009975">
+                <div class="card-body">
+                  <p style="color: #97999b">
+                    <b>Noticias</b>
+                  </p>
+                  <p style="color: black">Experiencia académica</p>
+                  <p style="color: #97999b">En las mentes de Óscar y Eduardo hay grandes sueños e ilusiones, ambos están convencidos que el único límite en la superación personal es el que se fija uno mismo, los avances tecnológicos encauzados al desarrollo y bienestar no solo estrecha los vínculos entre los seres humanos sino que también provee los recursos y las herramientas que abren un cúmulo inimaginable de posibilidades.</p>
+                  <p style="color: black">
+                    <a href="https://www.yucatan.com.mx/merida/experiencia-academica"><b>Conoce más</b></a>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <br><br>
     
             <div class="bs-example">
               <div class="card" style="max-width: 730px;">
