d0ea75f2bf67d79325ffc5c4ff01b7c63f236930
EnriqueOrozco <51837209+EnriqueOrozco@users.noreply.github.com>
2019-06-24 18:05:38 -0500

    Enrique

---
+++ instalacion de computadoras.php
@@ -0,0 +1,206 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>VEROSU S. de R.L M.I</title>
+		<link rel="stylesheet" href="">
+		<link rel="stylesheet" href="">
+		<meta charset="utf-8">
+  		<meta name="viewport" content="width=device-width, initial-scale=1">
+  		<link rel="stylesheet" type="text/css" href="css/login.css">
+  		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  		<link rel="shortcut icon" type="image/png" href="conphoto/ab.png"> 
+  		<link rel="stylesheet" href="css/register.css">
+	</head>
+	<body>
+	
+		<?php include "nav.html" ?>
+
+		<?php include "nav3info.html" ?>	
+
+		<div class="contenedor" align="center">
+    		<img class="img-fluid" src="conphoto/info25.jpg" width="1800px" style="opacity: 0.4">
+    		<div class="centrado">
+      			<p style="font-size: 60px; color: black;">
+      				<b>Ensamblaje de computadoras</b>
+      			</p>
+      			<p style="text-align: right; color: black;">En Verosu tenemos un equipo especialmente diseñado para el armado del equipo de computo dedicado a saber las mejores piezas y partes para que nuestra manquina podamos sacar el maximo provecho de estas.</p>
+    		</div>
+  		</div>
+
+  		<div class="container" style="margin-top: 30px">
+    	<div class="row">
+			  <div class="col-sm-8">
+				  <div class="container mt-3">
+					  <h2>
+						  Verosu S. de R.L M.I
+						  <small style="color: #76746E">
+							  <br>
+							  Servicios Integrales Comunicaciones y Tecnologia.
+						  </small>
+					    </h2>
+					    <br><br>
+					    <i class="fab fa-facebook-square" style="font-size: 48px; color: black"></i>
+					    <i class="fab fa-twitter-square" style="font-size: 48px; color: black"></i>
+					    <i class="fa fa-youtube-square" style="font-size: 48px; color: black"></i>
+				  </div>
+          <br><br>
+          <div class="bs-example">
+            <div class="card" style="max-width: 730px;">
+              <div class="row no-gutters">
+                <div class="col-md-8" style="background: #fff">
+                  <div class="card-body">
+                    <b>
+                      <p style="color: gray">Análisis</p>
+                    </b>
+                    <h5 class="card-title" style="color: black">Corrupción vs Honestidad</h5>
+                      <p class="card-text" style="color: black">En este e-book, ofrecemos nuestra perspectiva acerca de este flagelo y nuestro punto de vista para poder hacerle frente.</p>
+                      <a href="#" class="btn btn-light">Leer más</a>
+                  </div>
+                </div>
+                <div class="col-md-4" style="background: #868e96;">
+                  <img src="conphoto/1.jpeg" class="card-img-top h-100" alt="...">
+                </div>
+              </div>
+            </div>
+          </div>
+          <br><br>
+          <div class="card-deck">
+            <div class="card text-white ">
+              <div class="card-body">
+                <p style="color: #97999b">
+                  <b> Perspectivas</b>
+                </p>
+                <p style="color: black">La corrupción en México no es una cuestión cultural</p>
+                <p style="color: #97999b">Un problema demasiado grande para la actualidad</p>
+                <p style="color: black">
+                  <b>Leer más</b>
+                </p>
+              </div>
+            </div>
+            <div class="card text-white ">
+              <div class="card-body">
+                <p style="color: #97999b">
+                  <b>Perspectivas</b>
+                </p>
+                <p style="color: black">Empresas fantasma y licitaciones</p>
+                <p style="color: #97999b">¿Existe alguna solución?</p>
+                <p style="color: black">
+                  <b>Leer más</b>
+                </p>
+              </div>
+            </div>
+            <div class="card text-white">
+              <div class="card-body">
+                <p style="color: #97999b">
+                  <b>Perspectivas</b>
+                </p>
+                <p style="color: black">Corrupción al realizar trámites</p>
+                <p style="color: #97999b">Un obstáculo para las empresas en México</p>
+                <p style="color: black">
+                  <b>Leer más</b>
+                </p>
+              </div>
+            </div>
+          </div>
+          <br><br>
+          <div class="card-deck">
+            <div class="card text-white ">
+              <div class="card-body">
+                <p style="color: #97999b">
+                  <b>Análisis</b>
+                </p>
+                <p style="color: black">Delitos Financieros: detéctelo y resupelvalo</p>
+                <p style="color: #97999b">El delito financiero es un problema que exige a los directores corporativos y los ejecutivos de alto nivel, permanecer alertas, pues los...</p>
+                <p style="color: black">
+                  <b>Descargar guía</b>
+                </p>
+              </div>
+            </div>
+            <div class="card text-white ">
+              <div class="card-body">
+                <p style="color: #97999b">
+                  <b>Artículo</b>
+                </p>
+                <p style="color: black">LFPIORPI</p>
+                <p style="color: #97999b">El objetivo que la Ley Antilavado persigue es dar seguimiento al flujo de efectivo con el propósito de contar con una economía nacional más...</p>
+                <p style="color: black">
+                  <b>Descubre más</b>
+                </p>
+              </div>
+            </div>
+          </div>
+          <br><br>
+          <div class="bs-example">
+            <div class="card" style="max-width: 730px;">
+              <div class="row no-gutters">
+                <div class="col-md-8" style="background: #000">
+                  <div class="card-body">
+                    <b>
+                      <p style="color: gray">Perspectivas</p>
+                    </b>
+                    <h5 class="card-title" style="color: white">Eliminar la impunidad y la corrupción</h5>
+                      <p class="card-text" style="color: white">Erradicar la impunidad y la corrupción requiere un mejor sistema de procuración e impartición de justicia, así como un trabajo coordinado...</p>
+                      <a href="#" class="btn btn-light">Leer más</a>
+                  </div>
+                </div>
+                <div class="col-md-4" style="background: #868e96;">
+                  <img src="conphoto/1.jpeg" class="card-img-top h-100" alt="...">
+                </div>
+              </div>
+            </div>
+          </div>
+			  </div>
+			  <div class="col-sm-4">
+				  <div class="sub-cuerpo2">
+					  <br><br>
+					  <a class="twitter-timeline" data-lang="es" data-height="400" data-theme="light" data-link-color="#8C337A" href="https://twitter.com/TwitterDev/timelines/539487832448843776?ref_src=twsrc%5Etfw">National Park Tweets - Curated tweets by TwitterDev</a> 
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+				  </div>
+          <br>
+          <div class="container">
+            <div class="list-group">
+              <a href="contacto.php" class="list-group-item list-group-item-action" style="background-color: #59C3CC; color: white">
+                <b>Contacto</b>
+                <button type="" class="btn btn-light btn-sm btn-block">Escribenos a traves de nuestra solicitud en línea</button>
+              </a>
+              <br>
+              <a href="#" class="list-group-item list-group-item-action" style="background-color: #59C3CC; color: white">
+                <b>Solicitud de cotización</b>
+                <button type="" class="btn btn-light btn-sm btn-block">Enviar formulario electronico</button>
+              </a>
+              <br>
+              <a href="" class="list-group-item list-group-item-action" style="background-color: #59C3CC; color: white">
+                <b>Servicios</b>
+              </a>
+              <br>
+              <div class="card">
+                <div class="card-header" style="background-color: #59C3CC; color: white">
+                  <b>Temas Similares</b>
+                </div>
+                <div class="card-body">
+                  <a href="#">1</a>
+                  <br>
+                  <a href="#">2</a>
+                  <br>
+                  <a href="#">3</a>
+                  <br>
+                  <a href="#">4</a>
+                  <br>
+                  <a href="#">5</a>
+                </div>
+              </div>
+            </div>
+          </div>
+			  </div>    		
+    	</div>
+    </div>
+
+  		<?php include "footer.html" ?>
+
+	</body>
+</html>
