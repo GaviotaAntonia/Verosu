6f4abc5a7c8eec5ec91d39e7d65f6ef6e93f1554
Gaviota Antonia Herrera Prieto <35613309+GaviotaAntonia@users.noreply.github.com>
2019-06-24 12:16:52 -0500

    Solo hace falta modificaar hacer del despacho

---
+++ intcosas.php
@@ -0,0 +1,260 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>VEROSU S. de R.L M.I</title>
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  	<link rel="shortcut icon" type="image/png" href="conphoto/ab.png"> 
+    <link rel="stylesheet" href="css/masestilos.css">
+    <link rel="stylesheet" href="css/estilos.css">
+    <!--LETRA QUE SE UTLIZA-->
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    <style>
+      /* Make the image fully responsive */
+      .carousel-inner img {
+        width: 100%;
+        height: 100%;
+      }
+      #barra{
+        color: black;
+      }
+      .btn-group-vertical>.btn, .btn-group-vertical>.btn-group {
+        width: 188px;
+        }
+        .contenedor{
+          position: relative;
+          display: inline-block;
+          text-align: center;
+          width: 100%
+        }
+        .texto-encima{
+          position: absolute;
+          top: 10px;
+          left: 10px;
+          color: white;
+          }
+          .centrado{
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            color: White;
+          }
+    </style>
+
+  </head>
+  <body>
+    <!--NAV1-->
+    <?php include "nav.html"; ?>
+    <!--nav 2-->
+    <?php include 'nav2.html'; ?>
+    <!---FIN DE NAV BAR-->
+
+    <div class="contenedor" align="center">
+      <img class="img-fluid" src="conphoto/hm.jpg" width="1800px" style="opacity: 0.4">
+      <div class="centrado">
+        <p style="font-size: 60px; color: black;">
+          <b>Estrategia y Operaciones</b>
+          <p style="text-align: right; color: black;">Ayudamos a resolver los desafíos más difíciles y complejo que tienen los líderes de una organización, al brindarle una estrategia ejecutable que integre un profundo análisis, visión y conocimiento de  industria que genere valor duradero</p>
+      </div>
+    </div>
+    <div class="container" style="margin-top: 30px">
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="bs-example">
+            <div class="card" style="max-width: 730px;">
+              <div class="row no-gutters">
+                <div class="col-md-8" style="background: #fff">
+                  <div class="card-body"><b>
+                    <p style="color: gray">Articulos</p>
+                  </b>
+                  <h5 class="card-title" style="color: black">Industria 4.0</h5>
+                  <p class="card-text" style="color: black">La convergencia del mundo fisico y el digital</p>
+                  <a href="#" class="btn btn-light">Leer más</a>
+                </div>
+              </div>
+              <div class="col-md-4" style="background: #868e96;">
+                <img src="conphoto/industria4.jpg" class="card-img-top h-100" alt="...">
+              </div>
+            </div>
+          </div>
+        </div>
+            <br><br>
+        <div class="card-deck">
+          <div class="card text-white " style="border-top: solid 4px #009975">
+            <div class="card-body">
+              <p style="color: #97999b">
+                <b> Noticias</b>
+              </p>
+              <p style="color: black">MBA CASE COMPETION</p>
+              <p style="color: #97999b">Los bots han sido usados para llevar a cabo malas practicas, entre ellas, fraudes...</p>
+              <p style="color: black">
+                <b>Leer más</b>
+              </p>
+            </div>
+          </div>
+          <div class="card text-white " style="border-top: solid 4px #009975">
+            <div class="card-body">
+              <p style="color: #97999b">
+                <b>Perspectivas</b>
+              </p>
+              <p style="color: black">La ciberseguridad necesita escalar a la alta direccion empresarial</p>
+              <p style="color: #97999b">¿Existe alguna solución?</p>
+              <p style="color: black">
+                <b>Leer más</b>
+              </p>
+            </div>
+          </div>
+          <div class="card text-white" style="border-top: solid 4px #009975">
+            <div class="card-body">
+              <p style="color: #97999b">
+                <b>Noticias</b>
+              </p>
+              <p style="color: black">CiberEspionaje</p>
+              <p style="color: #97999b">La mejor manera de proteger a las organizaciones es con...</p>
+              <p style="color: black">
+                <b>Leer más</b>
+              </p>
+            </div>
+          </div>
+        </div>
+        <br><br>
+        <div class="card-deck">
+          <div class="card text-white ">
+            <div class="card-body">
+              <p style="color: #97999b">
+                <b>Artículo</b>
+              </p>
+              <p style="color: black">Nuevos horizontes en la generacion de ciber inteligencia</p>
+              <p style="color: #97999b">Conociendo a nuestros atacantes antes de que nos conozcan</p>
+              <p style="color: black">
+                <b>Descubre más</b>
+              </p>
+            </div>
+          </div>
+          <div class="card text-white ">
+            <div class="card-body">
+              <p style="color: #97999b">
+                <b>Servicios</b>
+              </p>
+              <p style="color: black">Riesgo Regulatorio</p>
+              <p style="color: #97999b">Esan enfocados a asistir a nuestro clientes para que puedan cumplir con sus objetivos de negocio sin incurrir en riesgos de cumplimiento</p>
+              <p style="color: black">
+                <b>Leer más</b>
+              </p>
+            </div>
+          </div>
+        </div>
+        <br><br>
+        <div class="bs-example">
+          <div class="card" style="max-width: 730px;">
+            <div class="row no-gutters">
+              <div class="col-md-8" style="background: #000">
+                <div class="card-body">
+                  <b>
+                    <p style="color: gray">Verosu en los medios</p>
+                  </b>
+                  <h5 class="card-title" style="color: white">Ciberseguridad:</h5>
+                  <p class="card-text" style="color: white">Los temas de seguridad de la informacion y ciberseguridad continuan apareciendo con...</p>
+                  <a href="#" class="btn btn-light">Leer más</a>
+                </div>
+              </div>
+              <div class="col-md-4" style="background: black;">
+                <img src="conphoto/1.jpeg" class="card-img-top h-100" alt="...">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+ <br>
+		
+
+
+
+
+			<div class="col-sm-4">
+        <div class="container">
+          <div class="list-group"><br>
+            <a href="contacto.php" class="list-group-item list-group-item-action" style="background-color: #58b4a6; color: black;"><b>Contacto</b><br>
+              <button type="" class="btn btn-light btn-sm btn-block">Escribenos a traves de nuestra solicitud en línea</button></a><br><br>
+                <a href="#" class="list-group-item list-group-item-action" style="background-color: #58b4a6; color: black;"><b>Solicitud de cotización</b><br><button type="" class="btn btn-light btn-sm btn-block">Enviar Formulario electronico</button></a><br><br>
+          </div>
+        </div>
+        <h3><br>SERVICIOS Y ANUNCIOS</h3>
+          <div id="accordion">
+            <div class="card">
+              <div class="card-header" style="background-color: #77daca">
+                <a class="card-link" data-toggle="collapse" href="#collapseOne" style="color: black">Estrategia</a>
+              </div>
+              <div id="collapseOne" class="collapse show" data-parent="#accordion">
+                <div class="card-body">
+                  <a href="estrategia.php" style="color: black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</a>
+                </div>
+              </div>
+            </div>
+
+                <div class="card">
+                  <div class="card-header" style="background-color: #77daca">
+                    <a class="collapsed card-link" data-toggle="collapse" href="#collapseTwo" style="color: black">Fusiones y adquisiciones</a>
+                  </div>
+                  <div id="collapseTwo" class="collapse" data-parent="#accordion">
+                    <div class="card-body">
+                      <a href="fusiones y adquisiciones.php" style="color: black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</a>
+                    </div>
+                  </div>
+                </div>
+                <div class="card">
+                  <div class="card-header" style="background-color: #77daca">
+                    <a class="collapsed card-link" data-toggle="collapse" href="#collapseThree" style="color: black">Finanzas</a>
+                  </div>
+                    <div id="collapseThree" class="collapse" data-parent="#accordion">
+                      <div class="card-body">
+                        <a href="finanzas-consultoria.php" style="color: black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</a>
+                      </div>
+                    </div>
+                </div>
+                <div class="card">
+                  <div class="card-header" style="background-color: #77daca">
+                    <a class="collapsed card-link" data-toggle="collapse" href="#collapsefour" style="color: black">Cadena de suministro</a>
+                  </div>
+                    <div id="collapsefour" class="collapse" data-parent="#accordion">
+                      <div class="card-body">
+                        <a href="cadenadesuministrocon.php" style="color: black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</a>
+                      </div>
+                    </div>
+                </div>
+                <div class="card">
+                  <div class="card-header" style="background-color: #77daca">
+                    <a class="collapsed card-link" data-toggle="collapse" href="#collapsefive" style="color: black">Transformacion de operaciones</a>
+                  </div>
+                    <div id="collapsefive" class="collapse" data-parent="#accordion">
+                      <div class="card-body">
+                        <a href="transformaciondeoperaciones.php" style="color: black">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</a>
+                      </div>
+                    </div>
+                </div>
+                <br>
+          </div>
+
+          <div class="card"><br>
+            <div class="card-header" style="background-color: gray; color: white"><b>Temas Similares</b></div>
+              <div class="card-body">
+                <a href="enbreveifrs.php">En Breve IFRS</a><br>
+                <a href="asesoriacontable.php">Asesoria Contable</a>
+                <br><a href="auditoriadeestadosfinancieros.php">Auditoria de Estados Financieros</a>
+                <br><a href="finanzascorporativas.php">Finanzas Corporativas</a>
+                <br><a href="auditoriainterna.php">Auditoria interna</a>
+              </div> 
+          </div>
+			</div>    		
+  </div></div>
+	<?php include "footer.html"; ?>
+	
+</body>
+</html>
