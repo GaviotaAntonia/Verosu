6af10b3c4a64f738d8694b6c17f35a524e428a8f
EnriqueOrozco <51837209+EnriqueOrozco@users.noreply.github.com>
2019-06-24 09:26:43 -0500

    Tecnologia

--- tecnologia.php
+++ tecnologia.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>VEROSU S. de R.L M.I</title>
+  <head>
+
+	  <meta charset="utf-8">
+	  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	  <title>VEROSU S. de R.L M.I</title>
   	<meta name="viewport" content="width=device-width, initial-scale=1">
   	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
   	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
@@ -11,90 +12,308 @@
   	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
   	<link rel="shortcut icon" type="image/png" href="conphoto/ab.png"> 
 
-	<link rel="stylesheet" href="css/masestilos.css">
-	<link rel="stylesheet" href="css/estilos.css">
-  <!--LETRA QUE SE UTLIZA-->
-	<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
-  <style>
-  /* Make the image fully responsive */
-  .carousel-inner img {
-    width: 100%;
-    height: 100%;
-  }
-  #barra{
-    color: black;
-  }
- .btn-group-vertical>.btn, .btn-group-vertical>.btn-group {
-    width: 188px;
-}
-.contenedor{
-    position: relative;
-    display: inline-block;
-    text-align: center;
-    width: 100%
-}
+	  <link rel="stylesheet" href="css/masestilos.css">
+	  <link rel="stylesheet" href="css/estilos.css">
+  
+    <!--LETRA QUE SE UTLIZA-->
+	  <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+	  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
+    
+    <style>
+      /* Make the image fully responsive */
+      .carousel-inner img 
+      {
+        width: 100%;
+        height: 100%;
+      }
+  
+      #barra
+      {
+        color: black;
+      }
  
-.texto-encima{
-    position: absolute;
-    top: 10px;
-    left: 10px;
-    color: white;
-}
-.centrado{
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+      .btn-group-vertical>.btn, .btn-group-vertical>.btn-group 
+      {
+        width: 188px;
+      }
+    
+      .contenedor
+      {
+        position: relative;
+        display: inline-block;
+        text-align: center;
+        width: 100%
+      }
+ 
+      .texto-encima
+      {
+        position: absolute;
+        top: 10px;
+        left: 10px;
+        color: white;
+      }
 
-    color: White;
-}
-  </style>
+      .centrado
+      {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        color: White;
+      }
+  
+    </style>
 
-</head>
-<body>
-  <!--NAV1-->
-  <?php include "nav.html"; ?>
-  <!--nav 2-->
-  <?php include 'nav2.html'; ?>
-<!---FIN DE NAV BAR-->
+  </head>
+  <body>
+    
+    <!--NAV1-->
+      <?php include "nav.html"; ?>
+    <!--nav 2-->
+      <?php include 'nav2.html'; ?>
+    <!---FIN DE NAV BAR-->
 
-
-
-
-	<div class="contenedor" align="center">
+	    <div class="contenedor" align="center">
         <img class="img-fluid" src="conphoto/106.jpeg" width="1800px" style="opacity: 0.4">
         <div class="centrado">
         	<p style="font-size: 60px; color: black;">
         		<b>Tecnología</b>
         	<p style="text-align: right; color: black;">Ayudamos a nuestros clientes a alcanzar sus objetivos, por ello en nuestros proyectos buscamos desarrollar, aplicar y proponer, los mejores modelos de negocio que les faciliten crecer de manera eficiente y rentable, apoyándonos para ello en las mejores tecnologías disponible</p>
         </div>
-    </div>
+      </div>
   
-          <?php include 'navbartech.html'; ?>
-    <div class="container" style="margin-top: 30px">
-    	<div class="row">
-			<div class="col-sm-8">
-				<div class="container mt-3">
-					<h2>
-						Verosu S. de R.L M.I
-						<small style="color: #76746E">
-							<br>
-							Servicios Integrales Comunicaciones y Tecnologia.
-						</small>
-					</h2>
-					<br><br>
-					<i class="fab fa-facebook-square" style="font-size: 48px; color: black"></i>
-					<i class="fab fa-twitter-square" style="font-size: 48px; color: black"></i>
-					<i class="fa fa-youtube-square" style="font-size: 48px; color: black"></i>
-				</div>
-			</div>
-			<div class="col-sm-4">
-				<div class="sub-cuerpo2">
-					<br><br>
-					<a class="twitter-timeline" data-lang="es" data-height="400" data-theme="light" data-link-color="#8C337A" href="https://twitter.com/TwitterDev/timelines/539487832448843776?ref_src=twsrc%5Etfw">National Park Tweets - Curated tweets by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-				</div>
-			</div>    		
+      <?php include 'navbartech.html'; ?>
+    
+      <div class="container" style="margin-top: 30px">
+    	  <div class="row">
+			    <div class="col-sm-8">
+				    <div class="container mt-3">
+					    <h2>
+						    Verosu S. de R.L M.I
+						    <small style="color: #76746E">
+							    <br>
+							    Servicios Integrales Comunicaciones y Tecnologia.
+						    </small>
+					    </h2>
+					    <br><br>
+					    <i class="fab fa-facebook-square" style="font-size: 48px; color: black"></i>
+					    <i class="fab fa-twitter-square" style="font-size: 48px; color: black"></i>
+					    <i class="fa fa-youtube-square" style="font-size: 48px; color: black"></i>
+				    </div>
+            <br><br>
+            <div class="bs-example">
+              <div class="card" style="max-width: 730px;">
+                <div class="row no-gutters">
+                  <div class="col-md-8" style="background: #000">
+                    <div class="card-body">
+                      <b>
+                        <p style="color: gray">Acerca de nosotros</p>
+                      </b>
+                      <h5 class="card-title" style="color: white">Alianzas tecnológicas de Verosu</h5>
+                      <p class="card-text" style="color: white">Estamos alineados con más de 30 empresas....</p>
+                      <a href="#" class="btn btn-light">Leer más</a>
+                    </div>
+                  </div>
+                  <div class="col-md-4" style="background: #868e96;">
+                    <img src="conphoto/1.jpeg" class="card-img-top h-100" alt="...">
+                  </div>
+                </div>
+              </div>
+            </div>
+            <br><br>
+            <div class="card-deck">
+              <div class="card text-white" style="border-top: solid 4px #009975">
+                <div class="card-body">
+                  <p style="color: #97999b">
+                    <b>Perspectivas</b>
+                  </p>
+                  <p style="color: black">Robots en la industria</p>
+                  <p style="color: #97999b">¿Amenaza necesidad u oportunidad laboral?</p>
+                  <p style="color: black">
+                    <b>Leer más</b>
+                  </p>
+                </div>
+              </div>
+              <div class="card text-white" style="border-top: solid 4px #009975">
+                <div class="card-body">
+                  <p style="color: #97999b">
+                    <b>Predcciones</b>
+                  </p>
+                  <p style="color: black">Cumplimiento de la Norma IFFRS16</p>
+                  <p style="color: #97999b">Nueva solución desarrollada por Verosu, plataforma SAP S/4HANA Real Estate</p>
+                  <p style="color: black">
+                    <b>Conoce más</b>
+                  </p>
+                </div>
+              </div>
+              <div class="card text-white" style="border-top: solid 4px #009975">
+                <div class="card-body">
+                  <p style="color: #97999b">
+                    <b>Articulo</b>
+                  </p>
+                  <p style="color: black">Inonovación SAP con Verosu</p>
+                  <p style="color: #97999b">Estamos listos para ayudar a las empresas a reimaginarlo todo</p>
+                  <p style="color: black">
+                    <b>Conoce más</b>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <br><br>
+            <div class="card-deck">
+              <div class="card text-white " style="border-top: solid 4px #009975">
+                <div class="card-body">
+                  <p style="color: #97999b">
+                    <b>Perspectivas</b>
+                  </p>
+                  <p style="color: black">SAP Enterprise Support</p>
+                  <p style="color: #97999b">Beneficios que Verosu brinda por el mantenimiento de sus licencias SAP</p>
+                  <p style="color: black">
+                    <b>Leer más</b>
+                  </p>
+                </div>
+              </div>
+              <div class="card text-white" style="border-top: solid 4px #009975">
+                <div class="card-body">
+                  <p style="color: #97999b">
+                    <b>Perspectivas</b>
+                  </p>
+                  <p style="color: black">Servicios de soporte AMS</p>
+                  <p style="color: #97999b">Diferencias de valor en los servicios de AMS con Verosu</p>
+                  <p style="color: black">
+                  <b>Conoce más</b>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <br><br>
+            <div class="card-deck">
+              <div class="card text-white" style="border-top: solid 4px #009975">
+                <div class="card-body">
+                  <p style="color: #97999b">
+                    <b>Articulo</b>
+                  </p>
+                  <p style="color: black">SAP C/4HANA</p>
+                  <p style="color: #97999b">Ofrecemos soluciones innovadoras y prácticas en torno a la gestión de riegos estratégicos, el manejo de crisis y los riesgos de marca y reputación</p>
+                  <p style="color: black">
+                    <b>Conozca más</b>
+                  </p>
+                </div>
+              </div>
+              <div class="card text-white" style="border-top: solid 4px #009975">
+                <div class="card-body">
+                  <p style="color: #97999b">
+                    <b>Análisis</b>
+                  </p>
+                  <p style="color: black">Robotics & Cognitive Automation (R&CA)</p>
+                  <p style="color: #97999b">Verous ofrece automatización en procesos para hacer negocios más eficiente</p>
+                  <p style="color: black">
+                    <b>Conoce más</b>
+                  </p>
+                </div>
+              </div>
+              <div class="card text-white" style="border-top: solid 4px #009975">
+                <div class="card-body">
+                  <p style="color: #97999b">
+                    <b>Análisis</b>
+                  </p>
+                  <p style="color: black">El ecosistema del Internet de las cosas</p>
+                  <p style="color: #97999b">Descubriendo el valor de los dispositivos para su negocio</p>
+                  <p style="color: black">
+                    <b>Conoce más</b>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <br><br>
+            <div class="card-deck">
+              <div class="card text-white" style="border-top: solid 4px #009975">
+                <div class="card-body">
+                  <p style="color: #97999b">
+                    <b>Análisis</b>
+                  </p>
+                  <p style="color: black">Nivel de madurez digital</p>
+                  <p style="color: #97999b">Talento y tecnologías en la era digital</p>
+                  <p style="color: black">
+                    <b>Conoce más</b>
+                  </p>
+                </div>
+              </div>
+              <div class="card text-white" style="border-top: solid 4px #009975">
+                <div class="card-body">
+                  <p style="color: #97999b">
+                    <b>Perspectivas</b>
+                  </p>
+                  <p style="color: black">Derecho al olvido en Internet</p>
+                  <p style="color: #97999b">¿Qué es cómo funciona en México?</p>
+                  <p style="color: black">
+                    <b>Conoce más</b>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <br><br>
+            <div class="bs-example">
+              <div class="card" style="max-width: 730px;">
+                <div class="row no-gutters">
+                  <div class="col-md-8" style="background: #000">
+                    <div class="card-body">
+                      <b>
+                        <p style="color: gray">Articulo</p>
+                      </b>
+                      <h5 class="card-title" style="color: white">Navegando la nueva brecha digital</h5>
+                      <p class="card-text" style="color: white">Capitalizando la influencia digital en el retail mexicano</p>
+                      <a href="#" class="btn btn-light">Leer más</a>
+                    </div>
+                  </div>
+                  <div class="col-md-4" style="background: #868e96;">
+                    <img src="conphoto/1.jpeg" class="card-img-top h-100" alt="...">
+                  </div>
+                </div>
+              </div>
+            </div>
+			    </div>
+			  <div class="col-sm-4">
+				  <div class="sub-cuerpo2">
+					  <br><br>
+					  <a class="twitter-timeline" data-lang="es" data-height="400" data-theme="light" data-link-color="#8C337A" href="https://twitter.com/TwitterDev/timelines/539487832448843776?ref_src=twsrc%5Etfw">National Park Tweets - Curated tweets by TwitterDev</a> 
+            <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+				  </div>
+          <div class="container">
+            <div class="list-group">
+              <a href="contacto.php" class="list-group-item list-group-item-action" style="background-color: #59C3CC; color: white">
+                <b>Contacto</b>
+                <button type="" class="btn btn-light btn-sm btn-block">Escribenos a traves de nuestra solicitud en línea</button>
+              </a>
+              <br>
+              <a href="#" class="list-group-item list-group-item-action" style="background-color: #59C3CC; color: white">
+                <b>Solicitud de cotización</b>
+                <button type="" class="btn btn-light btn-sm btn-block">Enviar formulario electronico</button>
+              </a>
+              <br>
+              <a href="" class="list-group-item list-group-item-action" style="background-color: #59C3CC; color: white">
+                <b>Servicios</b>
+              </a>
+              <br>
+              <div class="card">
+                <div class="card-header" style="background-color: #59C3CC; color: white">
+                  <b>Temas Similares</b>
+                </div>
+                <div class="card-body">
+                  <a href="#">1</a>
+                  <br>
+                  <a href="#">2</a>
+                  <br>
+                  <a href="#">3</a>
+                  <br>
+                  <a href="#">4</a>
+                  <br>
+                  <a href="#">5</a>
+                </div>
+              </div>
+            </div>
+          </div>
+			  </div>    		
     	</div>
     </div>
 
