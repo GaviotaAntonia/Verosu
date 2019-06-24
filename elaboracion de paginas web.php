2d268ca9d002a49867d8a575269e046fc6eb52e2
EnriqueOrozco <51837209+EnriqueOrozco@users.noreply.github.com>
2019-06-24 15:20:07 -0500

    Nuevas paginas

---
+++ elaboracion de paginas web.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>VEROSU S. de R.L M.I</title>
+		<link rel="stylesheet" href="">
+		<link rel="stylesheet" href="">
+		<meta charset="utf-8">
+  		<meta name="viewport" content="width=device-width, initial-scale=1">
+  		<link rel="stylesheet" type="text/css" href="css/login.css">
+  		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  		<link rel="shortcut icon" type="image/png" href="conphoto/ab.png"> 
+  		<link rel="stylesheet" href="css/register.css">
+	</head>
+	<body>
+	
+		<?php include "nav.html" ?>
+
+		<?php include "nav3info.html" ?>	
+
+		<div class="contenedor" align="center">
+    		<img class="img-fluid" src="conphoto/info21.png" width="1800px" style="opacity: 0.4">
+    		<div class="centrado">
+      			<p style="font-size: 60px; color: black;">
+      				<b>Investigación Forense</b>
+      			</p>
+      			<p style="text-align: right; color: black;">Ayudamos a las organizaciones a administrar sus áreas de riesgo financiero mediante la asistencia en el diseño y la implementación de gobierno, procesos, modelos, datos, tecnología y rendimiento de informes, incluyendo soporte durante todo el ciclo de vida de las operaciones</p>
+    		</div>
+  		</div>
+
+	</body>
+</html>
