ea294d1dd7de30c2662fb3d6d447df51e66a9ffc
EnriqueOrozco <51837209+EnriqueOrozco@users.noreply.github.com>
2019-06-25 09:10:07 -0500

    Ultimas paginas

---
+++ soporte.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>VEROSU S. de R.L M.I</title>
+		<link rel="stylesheet" href="">
+		<link rel="stylesheet" href="">
+		<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" type="text/css" href="css/login.css">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  	<link rel="shortcut icon" type="image/png" href="conphoto/ab.png"> 
+  	<link rel="stylesheet" href="css/register.css">
+	</head>
+	<body>
+	
+		<?php include "nav.html" ?>
+
+		<?php include "nav3info.html" ?>	
+
+    <div class="contenedor" align="center">
+      <img class="img-fluid" src="conphoto/info28.jpg" width="1800px" style="opacity: 0.4">
+      <div class="centrado">
+        <p style="font-size: 60px; color: black;">
+          <b>Soporte</b>
+        </p>
+        <p style="text-align: right; color: black;">Este es el soporte técnico donde podrán resolver sus dudas o algún problema relacionado con los sistemas o con la misma página o en otros casos ciertas fallas este apartado es para Contabilidad como para Informática.</p>
+      </div>
+    </div>
+
+    <div class="container" style="margin-top: 30px">
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="container mt-3">
+            <h2>
+              Verosu S. de R.L M.I
+              <small style="color: #76746E">
+                <br>
+                Servicios Integrales Comunicaciones y Tecnologia
+              </small>
+            </h2>
+            <br><br>
+            <i class="fab fa-facebook-square" style="font-size: 48px; color: black;"></i>
+            <i class="fab fa-twitter-square" style="font-size: 48px; color: black;"></i>
+            <i class="fa fa-youtube-square" style="font-size: 48px; color: black;"></i>
+          </div>
+          <div class="bs-example">
+            <div class="card" style="max-width: 730px">
+              <div class="row no-gutters">
+                <img src="conphoto/info29.jpg" class="card-img-top h-100" alt="...">
+                <div class="" style="background: #fff">
+                  <div class="card-body">
+                    <b>
+                      <p style="color: gray">Soporte</p>
+                    </b>
+                    <h5 class="card-title" style="color: black">Contabilidad</h5>
+                    <p class="casrd-text" style="color: black">Este chat es solo para problemas de contabilidad, ayuda sobre temas contables o alguna ayuda de manera presencial </p>
+                    <a href="#" class="btn btn-warning" style="color: black;"><b>Iniciar chat</b></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <br><br>
+          <div class="bs-example">
+            <div class="card" style="max-width: 730px">
+              <div class="row no-gutters">
+                <img src="conphoto/info30.jpg" class="card-img-top h-100" alt="...">
+                <div class="" style="background: #fff">
+                  <div class="card-body">
+                    <b>
+                      <p style="color: gray">Soporte</p>
+                    </b>
+                    <h5 class="card-title" style="color: black">Informática</h5>
+                    <p class="casrd-text" style="color: black">Este chat es solo para problemas de informática, para soluciones de algún sistema, para reportar algún problema o hacer un trabajo de manera presencial para el usuario/cliente.</p>
+                    <a href="#" class="btn btn-warning" style="color: black;"><b>Iniciar chat</b></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-4">
+          <div class="sub-cuerpo2">
+            <br><br>
+            <a class="twitter-timeline" data-lang="es" data-height="400" data-theme="dark" href="https://twitter.com/TwitterDev/timelines/539487832448843776?ref_src=twsrc%5Etfw">National Park Tweets - Curated tweets by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+          </div>
+          <br>
+          <div class="container">
+            <div class="list-group">
+              <a href="contacto.php" class="list-group-item list-group-item-action" style="background-color: #6b778d; color: white">
+                <b>Contacto</b>
+                <button type="" class="btn btn-light btn-sm btn-block">Escribenos a traves de nuestra solicitud en línea</button>
+              </a>
+              <br>
+              <a href="#" class="list-group-item list-group-item-action" style="background-color: #6b778d; color: white">
+                <b>Solicitud de cotización</b>
+                <button type="" class="btn btn-light btn-sm btn-block">Enviar formulario electronico</button>
+              </a>
+              <br>
+              <a href="" class="list-group-item list-group-item-action" style="background-color: #6b778d; color: white">
+                <b>Servicios</b>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  	<?php include "footer.html" ?>
+
+	</body>
+</html>
