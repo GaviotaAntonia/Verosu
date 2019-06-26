7978246a5c4cd42fbb57da0227960e4193ab311a
EnriqueOrozco <51837209+EnriqueOrozco@users.noreply.github.com>
2019-06-26 10:35:06 -0500

    Cambios

--- lenguajes mas utilizados.php
+++ lenguajes mas utilizados.php
@@ -42,7 +42,7 @@
 						<h2>Los 10 lenguajes más utilizados</h2>
 						<p>
 							<small style="color: #76746E; font-size: 15px">
-								Los lenguajes más utilizados en programación durante este año ha sido de los más buscados estos 10 lenguajes al momento de la programación esta grafica esta hecha por <b> HackerRank´s </b> esta se basa en las busquedas hechas sobre los lenguajes como tutoriales.
+								Los lenguajes más utilizados en programación durante este año ha sido de los más buscados estos 10 lenguajes al momento de la programación estas grafica estan medidas por el nivel de busquedas como de videos, se podra observar los lenguajes más buscados como los más utilizados este año dando en cuenta que no solo existen pocos lenguajes si no una gran variedad de ello
 							</small>
 						</p>
 						<br>
@@ -60,7 +60,7 @@
                 					  <b>Lenguajes más buscados</b>
                 					</h4>
                 					<br>
-                					<p style="color: #393e46;">En la tabla de alado se obseran los lenguajes más buscados en el 2019 en una parte esta grafica fue hecha por el grupo de <b> HackerRank´s </b> como se podra obeservar en la parte de abajo esta las 10 posiciones de mayor a menor buscado</p>
+                					<p style="color: #393e46;">En la tabla de alado se obseran los lenguajes más buscados en el 2019 esta grafica fue hecha por el grupo de <b> HackerRank´s </b> como se podra obeservar en la parte de abajo esta las 10 posiciones de mayor a menor buscado.</p>
                 					<br>
                 					<ol style="color: black">
                 						<li>Go</li>
@@ -80,8 +80,69 @@
           				</div>
           				<br>
           				<div class="card-deck">
-          					<img src="conphoto/info50.jpg">
+          					<img src="conphoto/info50.jpg" width="700">
+          					<div class="card text-white" style="border-top: solid 4px #ca3e47">
+          						<div class="card-body" style="text-align: justify">
+          							<h4 style="color: black; padding-top: 30px">
+          								<b>Lenguajes más utilizados</b>
+          							</h4>
+          							<br>
+          							<p style="color: #393e46">En la tabla se puede observar los lenguajes más utilizados en el 2019 abajo de esto se encontrar de mayor a menor los lenguajes más utilizados.</p>
+          							<br>
+          							<ol style="color: black">
+          								<li>Python</li>
+          								<li>Java</li>
+          								<li>JavaScript</li>
+          								<li>C#</li>
+          								<li>PHP</li>
+          								<li>C y C++</li>
+          								<li>R</li>
+          								<li>Objective-C</li>
+          								<li>Matlab</li>
+          								<li>TypeScript</li>
+          							</ol>
+          						</div>
+          					</div>
           				</div>
+					</div>
+					<br>
+					<div class="container mt3" style="text-align justify">
+						<br>
+						<p style="color: black">Como se llega a ver en las grficas los lenguajes han ido cambiado durante los años el porcentaje de popularidad ha bajado como ha subido estos lenguajes no son muy difíciles de aprender de hecho "nosotros tenemos algunos cursos de programación básica con un costo mínimo podrás acceder a ver nuestros cursos" te dejaremos abajo los links de cada lenguaje de programación para que los vallas probando.</p>
+						<br>
+						<div class="card-deck">
+							<div class="card text-white">
+								<span style="color: black; padding-top: 30px; padding-left: 30px"><b>Lenguajes más buscados en 2019</b></span>
+								<br><br>
+								<ol>
+									<li><a href="https://golang.org/">GO</a></li>
+									<li><a href="https://kotlinlang.org/">Kotlin</a></li>
+									<li><a href="https://www.python.org/">Python</a></li>
+									<li><a href="https://www.typescriptlang.org/">TypeScript</a></li>
+									<li><a href="https://www.r-project.org/">R</a></li>
+									<li><a href="https://www.scala-lang.org/">Scala</a></li>
+									<li><a href="https://swift.org/">Swift</a></li>
+									<li><a href="https://www.rust-lang.org/">Rust</a></li>
+									<li><a href="https://www.ruby-lang.org/es/">Ruby</a></li>
+									<li><a href="https://www.haskell.org/">Haskell</a></li>
+								</ol>
+							</div>
+							<div class="card text-white">
+								<span style="color: black; padding-top: 30px; padding-left: 30px"><b>Lenguajes más utilizados en 2019</b></span>
+								<br><br>
+								<ol>
+									<li><a href="https://www.python.org/">Python</a></li>
+									<li><a href="https://www.java.com/es/">Java</a></li>
+									<li><a href="https://developer.mozilla.org/es/docs/Web/JavaScript">JavaScript</a></li>
+									<li><a href="https://visualstudio.microsoft.com/es/?rr=https%3A%2F%2Fes.wikipedia.org%2F">C#</a></li>
+									<li><a href="https://www.php.net/">PHP</a></li>
+									<li><a href="https://isocpp.org/">C++</a></li>
+									<li><a href="https://www.r-project.org/">R</a></li>
+									<li><a href="https://www.mathworks.com/products/matlab.html">Matlab</a></li>
+									<li><a href="https://www.typescriptlang.org/">TypeScript</a></li>
+								</ol>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
---
+++ que es java.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>VEROSU S. de R.L M.I</title>
+        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/register.css">
+        <link rel="stylesheet" type="text/css" href="css/login.css">
+        <link rel="shortcut icon" type="image/png" href="conphoto/ab.png"> 
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    
+        <link rel="stylesheet" href="css/animate.css">
+    
+    </head>
+    <body>
+
+        <?php include "nav.html" ?>
+        <?php include "nav3info.html" ?>
+
+        <div class="contenedor" align="center">
+            <img src="conphoto/info51.jpg" class="img-fluid" width="1800px">
+        </div>
+
+        <div class="container" style="margin-top: 30px;">
+            <div class="row">
+                <div class="">
+                    <div  class="container mt3">
+                        <h4 style="color: gray">
+                            <b>
+                                Perspectivas
+                            </b>
+                        </h4>
+                        <h2>¿Qué es Java?</h2>
+                        <p>
+                            <small style="color: #76746E">
+                                Java a sido uno de los lengujaes más queridos como utilizados desde 2017 teniendo una popularidad ya que en los top de los lenguajes ha tenido la primera posición durante 2 años seguidos siendo uno de los idiomas más fuerte y tambien más utilizados en muchos otros lenguajes de programación.
+                            </small>
+                        </p>
+                        <br>
+                        <i class="fab fa-facebook-square" style="font-size: 48px; color: black"></i>
+                        <i class="fab fa-twitter-square" style="font-size: 48px; color: black"></i>
+                        <i class="fa fa-youtube-square" style="font-size: 48px; color: black"></i>
+                    </div>
+                    <br>
+                    <div class="container mt3" style="text-align: justify">
+                        <h3 style="color: black"><b>¿Qué es Java?</b></h3>
+                        <br>
+                        <span>"Java es un lenguaje de programacion con que se puede programar cualquier tipo de programa sin importar la dificultad teniendo en el ambito de internet siendo una parte general de la informatica".</span>
+                        <br><br>
+                        <span>Java es un lenguaje de programacion muy extendido ya que cubre varias necesidades tecnologicas más punteras, tomando en cuenta que sigue una logica al momento de programar esto se debe tomar en cuenta, Java no ocupa una etiqueta.</span>
+                        <br><br>
+                        <span>Java es tomado en cuenta como uno de los lenguajes de programación más relevantes de la epoca siendo por su fexibilidad o nobleza al momento de programar en ella.</span>
+                        <br><br>
+                        <h3 style="color: black"><b>¿Como llega a funcionar?</b></h3>
+                        <br>
+                        <span>"Para que Java en cuenta el codigo esto se basa al objeto que se quiere modifcar esto hace que se deba poner el nombre de la variable adentro del codigo".</span>
+                        <br><br>
+                        <span>Aquí la parte más importante de java es el lo que llegemos a escribir ya que aqui no se manejan por etiquitas si no por funcionalidad como se debe tomar en cuenta que esto se deberan cerrar o abrir ; punto y coma para finalizar la tarea.</span>
+                        <br><br>
+                        <div class="contenedor" align="center">
+                            <img src="conphoto/info52.png" class="img-fluid">
+                        </div>
+                        <br><br>
+                        <span>Solo debera tener mucho en cuenta que se debe cerrar ; punto y coma para que esto no tengan ningun problema al momento de quere ejecutar el codigo en una pagina web o en alguna aplicacion.</span>
+                        <br><br>
+                        <h3 style="color: black"><b>Historia de java</b></h3>
+                        <br>
+                        <span>Java fue creado en 1991 y publicado en 1995 por Sun Microsystem que fue adquirida por Oracle en el año 2010 con una gran intención de que los programadores escriban el codigo solo una vez y lo ejecutaron en cualquier dispositvo sin algun problema o error.</span>
+                        <br><br>
+                        <span>Esto es posible gracias a que Java cuenta con una JVM o Java Virtual Machine que brinda una gran probablilidad al lenguaje.</span>
+                        <br><br>
+                        <span>En el año 2016 la última versión estable de Java Standard Edition 8, la cual su fecha de lanzamiento fue en el año 2014, Java al ser un lenguaje derivado de C tiene unan sintaxis muy similares a este y en cuanto a su estructura, comienza con paquetes, dentro de los cuales estan las clases.</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <?php include "footer.html" ?>
+    
+    </body>
+
+</html>
