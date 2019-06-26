20dddd851cd302f8e3e19851560bdd05d0b35df5
Gaviota Antonia Herrera Prieto <35613309+GaviotaAntonia@users.noreply.github.com>
2019-06-26 14:36:23 -0500

    Agregar apartado para topografias de red

--- quesonlasredes.php
+++ quesonlasredes.php
@@ -52,17 +52,14 @@
     color: White;
 }
   </style>
-
 </head>
-
 <body>
   <!--NAV1-->
   <?php include "nav.html"; ?>
-
   <?php include 'nav3info.html'; ?>
-<!---FIN DE NAV BAR-->
-      <div class="contenedor" align="center">
-        <img class="img-fluid" src="conphoto/redes2.jpg" width="1800px" style="opacity: 0.4">
+  <!---FIN DE NAV BAR-->
+  <div class="contenedor" align="center">
+    <img class="img-fluid" src="conphoto/redes2.jpg" width="1800px" style="opacity: 0.4">
         <div class="centrado">
           <p style="font-size: 60px; color: black;">
             <b>¿Que son las redes?</b>
@@ -82,91 +79,116 @@
             <b>Medios de transmisión</b><br>
             <p style="color: black"><br>El medio empleado para transmitir información limita la velocidad de la red, la distancia eficaz entre ordenadores y la topología de la red. Los cables de cobre de dos hilos o los cables coaxiales proporcionan velocidades de transmisión de algunos miles de bps (bits por segundo) a largas distancias y de unos 100 Mbps (millones de bits por segundo) a corta distancia. Las fibras ópticas permiten velocidades de entre 100 y 1.000 Mbps a largas distancias. Por lo que se refiere a las redes inalámbricas, se puede lograr transferir datos a una velocidad de 720 Kbps en un rango de distancias entre 10 y 100 metros.</p>
           </div><br><br>
+          <div class="container"><br>
+            <!-- Nav tabs -->
+            <h5 style="text-align: center;"><small>Estructura de Redes</small></h5>
+            <ul class="nav nav-tabs justify-content-center" role="tablist">
+              <li class="nav-item">
+                <a class="nav-link active" data-toggle="tab" href="#home">El Software de Aplicaciones</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#menu1">El software de Red</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" data-toggle="tab" href="#menu2">El Hardware de Red</a>
+              </li>
+            </ul>
+            <!-- Tab panes -->
+            <div class="tab-content">
+              <div id="home" class="container tab-pane active"><br>
+                <h3 style="text-align: center;">El software de aplicaciones</h3>
+                <p style="text-align: center;color: black">programas que se comunican con los usuarios de la Red y permiten compartir información (como archivos, gráficos o vídeos) y recursos (como impresoras o unidades de disco).</p>
+              </div>
+              <div id="menu1" class="container tab-pane fade"><br>
+                <h3 style="text-align: center;">El software de red</h3>
+                <p style="text-align: center;color: black">Programas que establecen protocolos para que los ordenadores se comuniquen entre sí. Dichos protocolos se aplican enviando y recibiendo grupos de datos formateados denominados paquetes.</p>
+              </div>
+              <div id="menu2" class="container tab-pane fade"><br>
+                <h3 style="text-align: center;">El hardware de red</h3>
+                <p style="text-align: center;color: black"> formado por los componentes materiales que unen los Ordenadores. Dos componentes importantes son los medios de transmisión que transportan las señales de los ordenadores (típicamente cables o fibras ópticas) y el adaptador de red, que permite acceder al medio material que conecta a los ordenadores, recibir paquetes desde el software de red y transmitir instrucciones y peticiones a otros ordenadores.</p>
+              </div>
+            </div>
+          </div><br><br>
+          <p style="text-align: center;color: black;border-top: solid 4px #5985DB"><br>En resumen<br>Las redes están formadas por conexiones entre grupos de ordenadores y dispositivos asociados que permiten a los usuarios la transferencia electrónica de información. En estas estructuras, los diferentes ordenadores se denominan estaciones de trabajo y se comunican entre sí a través de un cable o línea telefónica conectada a los servidores.Dichos servidores son ordenadores como las estaciones de trabajo pero con funciones administrativas y están dedicados en exclusiva a supervisar y controlar el acceso a la red y a los recursos compartidos. Además de los ordenadores, los cables o la línea telefónica, existe en la red el Módem para permitir la transferencia de información convirtiendo las señales digitales a analógicas y viceversa, también existen en esta estructura los llamados Hubs y Switches con la función de llevar acabo la conectividad.</p>
           <div class="container">
-          <br>
-          <!-- Nav tabs -->
-          <h5 style="text-align: center;"><small>Estructura de Redes</small></h5>
-          <ul class="nav nav-tabs justify-content-center" role="tablist">
-            <li class="nav-item">
-              <a class="nav-link active" data-toggle="tab" href="#home">El Software de Aplicaciones</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" data-toggle="tab" href="#menu1">El software de Red</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" data-toggle="tab" href="#menu2">El Hardware de Red</a>
-            </li>
-          </ul>
-          <!-- Tab panes -->
-          <div class="tab-content">
-            <div id="home" class="container tab-pane active"><br>
-              <h3 style="text-align: center;">El software de aplicaciones</h3>
-              <p style="text-align: center;color: black">programas que se comunican con los usuarios de la Red y permiten compartir información (como archivos, gráficos o vídeos) y recursos (como impresoras o unidades de disco).</p>
-            </div>
-            <div id="menu1" class="container tab-pane fade"><br>
-              <h3 style="text-align: center;">El software de red</h3>
-              <p style="text-align: center;color: black">Programas que establecen protocolos para que los ordenadores se comuniquen entre sí. Dichos protocolos se aplican enviando y recibiendo grupos de datos formateados denominados paquetes.</p>
-            </div>
-            <div id="menu2" class="container tab-pane fade"><br>
-              <h3 style="text-align: center;">El hardware de red</h3>
-              <p style="text-align: center;color: black"> formado por los componentes materiales que unen los Ordenadores. Dos componentes importantes son los medios de transmisión que transportan las señales de los ordenadores (típicamente cables o fibras ópticas) y el adaptador de red, que permite acceder al medio material que conecta a los ordenadores, recibir paquetes desde el software de red y transmitir instrucciones y peticiones a otros ordenadores.</p>
-            </div>
+            <h2 style="text-align: center;"><small>Modelo OSI</small></h2>
+            <p>The .table-bordered class adds borders on all sides of the table and the cells:</p>            
+            <table class="table table-bordered" style="text-align: center;">
+              <thead>
+                <tr>
+                  <th style="background-color: #749AE1">#</th>
+                  <th style="background-color: #749AE1">Capas</th>
+                  <th style="background-color: #749AE1">Unidad de Intercambio</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td style="background-color: #E3EBF9">7</td>
+                  <td style="background-color: #E3EBF9">Aplicación</td>
+                  <td style="background-color: #E3EBF9">APDU</td>
+                </tr>
+                <tr>
+                  <td style="background-color: #E3EBF9">6</td>
+                  <td style="background-color: #E3EBF9">Presentación</td>
+                  <td style="background-color: #E3EBF9">PPDU</td>
+                </tr>
+                <tr>
+                  <td style="background-color: #E3EBF9">5</td>
+                  <td style="background-color: #E3EBF9">Sesión</td>
+                  <td style="background-color: #E3EBF9">SPDU</td>
+                </tr>
+                <tr>
+                  <td style="background-color: #E3EBF9">4</td>
+                  <td style="background-color: #E3EBF9">Transporte</td>
+                  <td style="background-color: #E3EBF9">TPDU</td>
+                </tr>
+                <tr>
+                  <td style="background-color: #E3EBF9">3</td>
+                  <td style="background-color: #E3EBF9">Red</td>
+                  <td style="background-color: #E3EBF9">Paquete de Red</td>
+                </tr>
+                <tr>
+                  <td style="background-color: #E3EBF9">2</td>
+                  <td style="background-color: #E3EBF9">Enlace de Datos</td>
+                  <td style="background-color: #E3EBF9">Trama de red</td>
+                </tr>
+                <tr>
+                  <td style="background-color: #E3EBF9">1</td>
+                  <td style="background-color: #E3EBF9">Fisica</td>
+                  <td style="background-color: #E3EBF9">Bit</td>
+                </tr>
+              </tbody>
+            </table>
           </div>
-        </div><br><br>
-        <p style="text-align: center;color: black;border-top: solid 4px #5985DB"><br>En resumen<br>Las redes están formadas por conexiones entre grupos de ordenadores y dispositivos asociados que permiten a los usuarios la transferencia electrónica de información. En estas estructuras, los diferentes ordenadores se denominan estaciones de trabajo y se comunican entre sí a través de un cable o línea telefónica conectada a los servidores.Dichos servidores son ordenadores como las estaciones de trabajo pero con funciones administrativas y están dedicados en exclusiva a supervisar y controlar el acceso a la red y a los recursos compartidos. Además de los ordenadores, los cables o la línea telefónica, existe en la red el Módem para permitir la transferencia de información convirtiendo las señales digitales a analógicas y viceversa, también existen en esta estructura los llamados Hubs y Switches con la función de llevar acabo la conectividad.</p>
-        <div class="container">
-        <h2 style="text-align: center;"><small>Modelo OSI</small></h2>
-        <p>The .table-bordered class adds borders on all sides of the table and the cells:</p>            
-        <table class="table table-bordered" style="text-align: center;">
-          <thead>
-          <tr>
-              <th style="background-color: #749AE1">#</th>
-              <th style="background-color: #749AE1">Capas</th>
-              <th style="background-color: #749AE1">Unidad de Intercambio</th>
-          </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td style="background-color: #E3EBF9">7</td>
-              <td style="background-color: #E3EBF9">Aplicación</td>
-              <td style="background-color: #E3EBF9">APDU</td>
-            </tr>
-            <tr>
-              <td style="background-color: #E3EBF9">6</td>
-              <td style="background-color: #E3EBF9">Presentación</td>
-              <td style="background-color: #E3EBF9">PPDU</td>
-            </tr>
-            <tr>
-              <td style="background-color: #E3EBF9">5</td>
-              <td style="background-color: #E3EBF9">Sesión</td>
-              <td style="background-color: #E3EBF9">SPDU</td>
-            </tr>
-            <tr>
-              <td style="background-color: #E3EBF9">4</td>
-              <td style="background-color: #E3EBF9">Transporte</td>
-              <td style="background-color: #E3EBF9">TPDU</td>
-            </tr>
-            <tr>
-              <td style="background-color: #E3EBF9">3</td>
-              <td style="background-color: #E3EBF9">Red</td>
-              <td style="background-color: #E3EBF9">Paquete de Red</td>
-            </tr>
-            <tr>
-              <td style="background-color: #E3EBF9">2</td>
-              <td style="background-color: #E3EBF9">Enlace de Datos</td>
-              <td style="background-color: #E3EBF9">Trama de red</td>
-            </tr>
-            <tr>
-              <td style="background-color: #E3EBF9">1</td>
-              <td style="background-color: #E3EBF9">Fisica</td>
-              <td style="background-color: #E3EBF9">Bit</td>
-            </tr>
-          </tbody>
-        </table>
-      </div>
+            <br><br>
+            <div class="card-deck">
+              <div class="card text-white " style="border-top: solid 4px #009975">
+                <div class="card-body animated ">
+                  <p style="color: #97999b">
+                    <b>Noticias</b>
+                  </p>
+                  <p style="color: black">EEUU Lanza ciberataque contra Íran</p>
+                  <p style="color: #97999b">La operación fue una recomendación del Pentágono para deteriorar el funcionamiento de la red que controla los misiles iraníes y para reducir las capacidades ofensivas informáticas de la Guardia Revolucionaria Islámica.</p>
+                  <p style="color: black">
+                    <a href="http://www.redusers.com/noticias/eeuu-lanzo-ciberataque-iran/"><b>Leer más</b></a>
+                  </p>
+                </div>
+              </div>
+              <div class="card text-white" style="border-top: solid 4px #009975">
+                <div class="card-body animated ">
+                  <p style="color: #97999b">
+                    <b>Noticias</b>
+                  </p>
+                  <p style="color: black">La comunidad andaluza, entre las tres peor protegidas ante ataques informáticos</p>
+                  <p style="color: #97999b">En el primer trimestre se detectaron 130.000 problemas de seguridad que afectan a empresas y ciudadanos</p>
+                  <p style="color: black">
+                  <a href="https://www.larazon.es/local/andalucia/la-comunidad-andaluza-entre-las-tres-peor-protegidas-ante-ataques-informaticos-EI23900613"><b>Conoce más</b></a>
+                  </p>
+                </div>
+              </div>
+            </div>
 
- </div>
-
+        </div>
 
 
       <div class="col-sm-4">          
