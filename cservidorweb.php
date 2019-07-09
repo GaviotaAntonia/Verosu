59d09371abd95e1bc98c17321b84339347d8a5fc
Yanet0187 <52713013+Yanet0187@users.noreply.github.com>
2019-07-09 17:44:52 -0500

    servidor web

---
+++ cservidorweb.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<title>VEROSU S. de R.L M.I</title>
+		<link rel="stylesheet" href="">
+		<link rel="stylesheet" href="">
+		<meta charset="utf-8">
+  	<meta name="viewport" content="width=device-width, initial-scale=1">
+  	<link rel="stylesheet" type="text/css" href="css/login.css">
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  	<link rel="shortcut icon" type="image/png" href="conphoto/ab.png"> 
+  	<link rel="stylesheet" href="css/register.css">
+  		<meta name="viewport" content="width=device-width, initial-scale=1">
+  		<link rel="stylesheet" type="text/css" href="css/login.css">
+  		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
+  		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  		<link rel="shortcut icon" type="image/png" href="conphoto/ab.png"> 
+  		<link rel="stylesheet" href="css/register.css">
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css">
+    <link rel="stylesheet" href="css/animate.css">
+	</head>
+	<body>
+	
+		<?php include "nav.html" ?>
+
+		<?php include "nav3info.html" ?>	
+
+		<div class="contenedor" align="center">
+    	<img class="img-fluid" src="conphoto/servidor.jpg" width="1800px" style="opacity: 0.4">
+    	<div class="centrado">
+      	<p style="font-size: 60px; color: black;">
+      		<b><!--Cotización de servidor para páginas web--></b>
+      	</p>
+      	<p style="text-align: right; color: black;"><!--¿Estás pensando en poner en marcha tu propia página web? Seguro que te vienen muchas preguntas relacionadas con los servicios que vas a necesitar, en Verosu tenemos los mejores precios del mercado.--></p>
+    	</div>
+  	</div>
+
+  	<div class="container" style="margin-top: 30px">
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="container mt-3">
+            <h2>Verosu S. de R.L M.I<small style="color: #76746E"><br>Servicios Integrales Comunicaciones y Tecnologia.</small></h2><br><br>
+					    <i class="fab fa-facebook-square" style="font-size: 48px; color: black"></i>
+					    <i class="fab fa-twitter-square" style="font-size: 48px; color: black"></i>
+					    <i class="fa fa-youtube-square" style="font-size: 48px; color: black"></i>
+				  </div>
+
+          <br><br>
+          <div class="bs-example">
+            <div class="card" style="max-width: 730px;">
+              <div class="row no-gutters">
+                <img src="conphoto/servidor2.jpg" class="card-img-top h-100" alt="...">
+                <div class="" style="background: #fff">
+                  <div class="card-body"><b><p style="color: gray">¿Qué es el hosting web o alojamiento web?</p></b>
+                    <h5 class="card-title" style="color: black"></h5>
+                      <p class="card-text" style="color: black">"Por lo tanto, llamamos hosting o alojamiento web al servicio de almacenamiento que proporcionan estos servidores. Y los proveedores de hosting web, ofrecen ese espacio para alojar tus páginas web y que puedan estar disponibles las 24h del día y desde cualquier parte del mundo".</p>
+                      <a href="#" class="btn btn-warning"><b>Leer más</b></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <br><br>
+          <div class="bs-example">
+            <div class="card" style="max-width: 730px;">
+              <div class="row no-gutters">
+                <img src="conphoto/servidor1.jpg" class="card-img-top h-100" alt="...">
+                <div class="" style="background: #fff">
+                  <div class="card-body"><b><p style="color: gray">Análisis</p></b>
+                    <h5 class="card-title" style="color: black"></h5>
+                      <p class="card-text" style="color: black">Los servidores web son los pilares del Hosting, permiten a los clientes alquilar almacenamiento en servidores para alojar sus archivos, conoce aquí los servidores más usados.</p>
+                      <a href="#" class="btn btn-warning"><b>Leer más</b></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <br><br>
+          <div class="card-deck">
+            <div class="card text-white " style="border-top:  solid 4px #900048">
+              <div class="card-body">
+                <p style="color: #97999b">
+                  <b> Artículo</b>
+                </p>
+                <p style="color: black">¿Qué tipos de hosting hay?</p>
+                <p style="color: #97999b">Aqui te decimos cual tipo se adapta mejor a tus necesidades en esta neueva era de la información. </p>
+                <p style="color: black">
+                  <b>Leer más</b>
+                </p>
+              </div>
+            </div>
+
+            <div class="card text-white " style="border-top:  solid 4px #900048">
+              <div class="card-body">
+                <p style="color: #97999b">
+                  <b>Análisis</b>
+                </p>
+                <p style="color: black">Cotización de Servidor</p>
+                <p style="color: #97999b">En Verosu cuenta con la cotización de diferentes servidores web para que el cliente pueda elegir el que mas se acomode a sus necesidades.</p>
+                <p style="color: black">
+                  <b>Leer más</b>
+                </p>
+              </div>
+            </div>
+
+            <div class="card text-white " style="border-top:  solid 4px #900048">
+              <div class="card-body">
+                <p style="color: #97999b">
+                  <b>Noticia</b>
+                </p>
+                <p style="color: black">Top de los servidores web más utilizaos en el mundo</p>
+                <p style="color: #97999b">Cuando ingresamos a internet y abrimos una página nuestro navegador web, el navegador se está conectando a un servidor remoto y solicitando que le envíe la página que queremos ver, este servicio es un servidor web.</p>
+                <p style="color: black">
+                  <b>Leer más</b>
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <br><br>
+          <div class="bs-example">
+            <div class="card" style="max-width: 730px;">
+              <div class="row no-gutters">
+                <img src="conphoto/info42.png" class="card-img-top h-100" alt="...">
+                <div class="" style="background: #000">
+                  <div class="card-body">
+                    <b>
+                      <p style="color: gray">Perspectivas</p>
+                    </b>
+                    <h5 class="card-title" style="color: white">¿La seguridad de tu página web puede correr peligro?</h5>
+                      <p class="card-text" style="color: white">Para Verosu lo más importante es la seguridad de nuestros clientes, asi que ofrecemos protección y respaldo de los servidores para poder brindar el mejor servicio.</p>
+                      <a href="#" class="btn btn-warning"><b>Leer más</b></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+			 <div class="col-sm-4">
+          <div class="sub-cuerpo2">
+            <br><br>
+            <a class="twitter-timeline" data-lang="es" data-height="400" data-theme="dark" href="https://twitter.com/TwitterDev/timelines/539487832448843776?ref_src=twsrc%5Etfw">National Park Tweets - Curated tweets by TwitterDev</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+          </div>
+          <br>
+          <div class="container">
+            <div class="list-group">
+              <a href="contacto.php" class="list-group-item list-group-item-action" style="background-color: #6b778d; color: white">
+                <b>Contacto</b>
+                <button type="" class="btn btn-light btn-sm btn-block">Escribenos a traves de nuestra solicitud en línea</button>
+              </a>
+              <br>
+              <a href="#" class="list-group-item list-group-item-action" style="background-color: #6b778d; color: white">
+                <b>Solicitud de cotización</b>
+                <button type="" class="btn btn-light btn-sm btn-block">Enviar formulario electronico</button>
+              </a>
+              <br>
+              <a href="" class="list-group-item list-group-item-action" style="background-color: #6b778d; color: white">
+                <b>Servicios</b>
+              </a>
+              <br>
+              <div class="card">
+                <div class="card-header" style="background-color: #6b778d; color: white">
+                  <b>Temas Similares</b>
+                </div>
+                <div class="card-body">
+                  <a href="#">1</a>
+                  <br>
+                  <a href="#">2</a>
+                  <br>
+                  <a href="#">3</a>
+                  <br>
+                  <a href="#">4</a>
+                  <br>
+                  <a href="#">5</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>   
+
+    	</div>
+    </div>
+
+  		<?php include "footer.html" ?>
+
+	</body>
+</html>
